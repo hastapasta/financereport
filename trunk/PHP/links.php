dfd416a9e82d9e9c45e8a174fddf97ea2ed2feb0
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-04-19 16:08:42 +0000

    Changed path of one of the links.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@44 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/links.php
+++ trunk/PHP/links.php
@@ -6,7 +6,7 @@
 <A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=yahoo_ge_qtr_net_income&insert=1">data_grab_v2?Data_Set=yahoo_ge_qtr_net_income&insert=1</A><BR>
 <A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=yahoo_shares_outstand&insert=0">data_grab_v2?Data_Set=yahoo_shares_outstand&insert=1</A><BR>
 <A HREF="http://win-d2sjsg6emdd/dev/InitialLoad.php">Initial Load</A><BR>
-<A HREF="http://win-d2sjsg6emdd/dev/DisplayDataSet.php">Display Data Set</A><BR>
+<A HREF="http://win-d2sjsg6emdd/DisplayDataSet.php">Display Data Set</A><BR>
 <BR>
 <A HREF="http://win-d2sjsg6emdd/phpmyadmin">phpmyadmin</A><BR>
 <BR>
