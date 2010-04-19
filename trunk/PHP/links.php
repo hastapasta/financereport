187423700d0614e4c066ffe4357b49b24434fa27
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-04-19 13:53:09 +0000

    Added new (dev) directory structure.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@43 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/links.php
+++ trunk/PHP/links.php
@@ -1,14 +1,12 @@
 <HTML>
 	<BODY>
-<A HREF="http://win-d2sjsg6emdd/regex_find_offset.php">regex_find_offset</A><BR>
-<A HREF="http://win-d2sjsg6emdd/data_grab.php">data_grab</A><BR>
-<A HREF="http://win-d2sjsg6emdd/data_grab.php?Data_Set=Initial_UE_Claims&insert=0">data_grab?Data_Set=Initial_UE_Claims&insert=0</A><BR>
-<A HREF="http://win-d2sjsg6emdd/data_grab.php?Data_Set=Initial_UE_Claims&insert=1">data_grab?Data_Set=Initial_UE_Claims&insert=1</A><BR>
-<A HREF="http://win-d2sjsg6emdd/data_grab_v2.php?Data_Set=URL_Initial_UE_Claims&insert=1">data_grab_v2?Data_Set=URL_Initial_UE_Claims&insert=1</A><BR>
-<A HREF="http://win-d2sjsg6emdd/data_grab_v2.php?Data_Set=yahoo_ge_qtr_net_income&insert=1">data_grab_v2?Data_Set=yahoo_ge_qtr_net_income&insert=1</A><BR>
-<A HREF="http://win-d2sjsg6emdd/data_grab_v2.php?Data_Set=yahoo_shares_outstand&insert=0">data_grab_v2?Data_Set=yahoo_shares_outstand&insert=1</A><BR>
-<A HREF="http://win-d2sjsg6emdd/InitialLoad.php">Initial Load</A><BR>
-<A HREF="http://win-d2sjsg6emdd/DisplayDataSet.php">Display Data Set</A><BR>
+<A HREF="http://win-d2sjsg6emdd/dev/regex_find_offset.php">regex_find_offset</A><BR>
+
+<A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=URL_Initial_UE_Claims&insert=1">data_grab_v2?Data_Set=URL_Initial_UE_Claims&insert=1</A><BR>
+<A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=yahoo_ge_qtr_net_income&insert=1">data_grab_v2?Data_Set=yahoo_ge_qtr_net_income&insert=1</A><BR>
+<A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=yahoo_shares_outstand&insert=0">data_grab_v2?Data_Set=yahoo_shares_outstand&insert=1</A><BR>
+<A HREF="http://win-d2sjsg6emdd/dev/InitialLoad.php">Initial Load</A><BR>
+<A HREF="http://win-d2sjsg6emdd/dev/DisplayDataSet.php">Display Data Set</A><BR>
 <BR>
 <A HREF="http://win-d2sjsg6emdd/phpmyadmin">phpmyadmin</A><BR>
 <BR>
