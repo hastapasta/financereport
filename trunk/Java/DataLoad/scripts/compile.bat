7b7f5f7e5b412692085c17c4922deb8927153790
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-04-20 01:03:40 +0000

    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@56 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

---
+++ trunk/Java/DataLoad/scripts/compile.bat
@@ -0,0 +1,5 @@
+set javahome=C:\progra~1\java\jdk1.6.0_18
+set classpath=.;%javahome%;D:\dev\java\ServiceTest
+set PATH=%PATH%;%javahome%\bin
+rem javac -classpath "%classpath%" com\roeschter\jsl\DataLoad.java
+javac -classpath "%classpath%" com\roeschter\jsl\DataGrab.java
