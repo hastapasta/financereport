2e59aad3aaa105b37e5dec9c89a88e43855b84aa
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-04-27 19:44:21 +0000

    Initial setup of compile2.bat menu interface
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@80 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/DataLoad/scripts/compile.bat
+++ trunk/Java/DataLoad/scripts/compile.bat
@@ -1,5 +1,18 @@
+@echo on
 set javahome=C:\progra~1\java\jdk1.6.0_18
 set classpath=.;%javahome%;D:\dev\java\ServiceTest
-set PATH=%PATH%;%javahome%\bin
-rem javac -classpath "%classpath%" com\roeschter\jsl\DataLoad.java
+PATH=%PATH%;%javahome%\bin
+if "%1"=="All" goto all
+javac -classpath "%classpath%" com\roeschter\jsl\%1
+goto end
+
+
+
+
+:all
+javac -classpath "%classpath%" com\roeschter\jsl\DataLoad.java
 javac -classpath "%classpath%" com\roeschter\jsl\DataGrab.java
+javac -classpath %classpath% com\roeschter\jsl\ProcessingFunctions.java
+javac -classpath %classpath% com\roeschter\jsl\UtilityFunctions.java
+
+:end
