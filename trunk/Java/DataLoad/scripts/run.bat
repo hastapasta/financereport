d7144e8ba4aade4a15d0ebc9c60a44fed1e6a557
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-04-27 03:27:57 +0000

    Added flag for standard error output.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@69 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/DataLoad/scripts/run.bat
+++ trunk/Java/DataLoad/scripts/run.bat
@@ -1,4 +1,4 @@
 set javahome=C:\progra~1\java\jdk1.6.0_18
 set classpath=.;C:\dev\java\ServiceTest;%javahome%;C:\dev\java\ServiceTest\com\roeschter\jsl;C:\progra~1\MySQL\mysql-connector-java-5.1.12-bin.jar
 set PATH=%PATH%;%javahome%\bin
-java -classpath "%classpath%" -Dservice.inifile=C:\dev\java\ServiceTest\jsl.ini com.roeschter.jsl.DataLoad
+java -classpath "%classpath%" -Dservice.inifile=C:\dev\java\ServiceTest\jsl.ini com.roeschter.jsl.DataLoad 2>error.log
