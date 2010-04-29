2d33710edcf69a83f75f00c0ff7b6eddca3a045f
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-04-29 11:10:50 +0000

    Added the VerifyData link
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@81 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/links.php
+++ trunk/PHP/links.php
@@ -7,6 +7,7 @@
 <A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=yahoo_shares_outstand&insert=0">data_grab_v2?Data_Set=yahoo_shares_outstand&insert=1</A><BR>
 <A HREF="http://win-d2sjsg6emdd/dev/InitialLoad.php">Initial Load</A><BR>
 <A HREF="http://win-d2sjsg6emdd/dev/DisplayDataSet.php">Display Data Set</A><BR>
+<A HREF="http://win-d2sjsg6emdd/dev/VerifyData.php?frame=parent">Verify Data</A><BR>
 <BR>
 <A HREF="http://win-d2sjsg6emdd/phpmyadmin">phpmyadmin</A><BR>
 <BR>
