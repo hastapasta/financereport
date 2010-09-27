e51d6fe868a7220b64d760d9a9945af49881fb78
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-09-27 17:46:18 +0000

    Added code to toggle view_fact_data view
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@123 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/VerifyData.php
+++ trunk/PHP/VerifyData.php
@@ -21,10 +21,11 @@ else if ($frame == 'internaldata')
 ?>
 <table>
 <?php
+	mysql_connect("127.0.0.1:3306", "root", "madmax1.") or die(mysql_error());
+	mysql_select_db("mydb") or die(mysql_error());
 	if(isset($_POST['submit_internal'])) 
 	{
-		mysql_connect("127.0.0.1:3306", "root", "madmax1.") or die(mysql_error());
-		mysql_select_db("mydb") or die(mysql_error());
+		
 		$selected_data_set = $_POST['data_set'];
 		
 		$query1 = //"select data_set,ticker,value,static_url from view_fact_data,extract_info where view_fact_data.data_set=extract_info.Data_Set AND data_set='".$selected_data_set."'";
@@ -51,6 +52,36 @@ else if ($frame == 'internaldata')
 	
 	
 }
+else if(isset($_POST['submit_table_switch'])) 
+{
+		$query1 = "select count(primary_key) from view_fact_data";
+		$query2 = "select count(primary_key) from fact_data_stage";
+		
+		$result1 = mysql_query($query1) or die("Failed Query of " . $query1);
+		$row1 = mysql_fetch_array($result1);
+		$result2 = mysql_query($query2) or die("Failed Query of " . $query2);
+		$row2 = mysql_fetch_array($result2);
+		
+		//echo "<TR><TD>".$row1['count(primary_key)']."</TD>   <TD>".$row2['count(primary_key)']."</TD></TR>";
+		mysql_query("DROP VIEW view_fact_data") or die("Failed to drop view_fact_data");
+		
+		//previous view was based off of fact_data_stage, create new view off of fact_data
+		if ($row1['count(primary_key)'] == $row2['count(primary_key)'])
+		{
+				mysql_query("CREATE ALGORITHM = UNDEFINED VIEW `view_fact_data` AS SELECT * FROM `fact_data` ") or die("Failed to create view_fact_data");
+				echo "CREATED NEW VIEW FROM fact_data";
+		}
+		//create new view off of fact_data_stage
+		else
+		{
+			mysql_query("CREATE ALGORITHM = UNDEFINED VIEW `view_fact_data` AS SELECT * FROM `fact_data_stage` ") or die("Failed to create view_fact_data");
+			echo "CREATED NEW VIEW FROM fact_data_stage";
+		}
+		
+		
+		
+		
+}
 
 ?>
 </table>
@@ -73,7 +104,9 @@ else if ($frame == 'form')
 	mysql_connect("127.0.0.1:3306", "root", "madmax1.") or die(mysql_error());
 	mysql_select_db("mydb") or die(mysql_error());
 ?>
-
+<table>
+<tr>
+	<td>
 <form name="SelectFactData" action="VerifyData.php?frame=internaldata" method=POST target=internaldata> 	
 					<table>
 						<tr><td>
@@ -100,6 +133,47 @@ else if ($frame == 'form')
 							</table>
 							
 </form>
+</td>
+<td>
+	                                 
+	                               </td>
+<td>
+
+<form name="SelectFactData" action="VerifyData.php?frame=internaldata" method=POST target=internaldata> 	
+					<table>
+						<tr><td>
+<?php
+		$query1 = "select count(primary_key) from view_fact_data";
+		$query2 = "select count(primary_key) from fact_data_stage";
+		
+		$result1 = mysql_query($query1) or die("Failed Query of " . $query1);
+		$row1 = mysql_fetch_array($result1);
+		$result2 = mysql_query($query2) or die("Failed Query of " . $query2);
+		$row2 = mysql_fetch_array($result2);
+		
+		//echo "<TR><TD>".$row1['count(primary_key)']."</TD>   <TD>".$row2['count(primary_key)']."</TD></TR>";
+		
+		if ($row1['count(primary_key)'] == $row2['count(primary_key)']) 
+			echo "Current view: fact_data_stage";
+		else
+			echo "Current view: fact_data";
+			
+		
+	
+
+?>						
+						
+						</td>
+						<td>
+							<input type="submit" value="Toggle Fact View" name="submit_table_switch" 
+							onclick="SelectFactData.action='VerifyData.php?frame=internaldata'; return true" >
+						</td></tr>
+							</table>
+							
+</form>
+</td>
+</tr>
+</table>
 
 <?php
 }
