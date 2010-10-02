ebc8150ccac3808d2261f4f135da51b3ed600647
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-10-02 03:48:00 +0000

    -Added the ticker input control and added a dynamic filter. -removed the toggle view control into a separate php file
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@133 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/VerifyData.php
+++ trunk/PHP/VerifyData.php
@@ -6,7 +6,7 @@ if ($frame == 'parent')
 ?>
 
 
-<frameset rows="20%,*">
+<frameset rows="30%,*">
 	<frame src="http://win-d2sjsg6emdd/dev/VerifyData.php?frame=form" name=form>
 <frameset cols="50%,*">
  	<frame src="http://win-d2sjsg6emdd/dev/VerifyData.php?frame=internaldata" name=internaldata>
@@ -19,6 +19,8 @@ if ($frame == 'parent')
 else if ($frame == 'internaldata')
 {
 ?>
+<html>
+	<body>
 <table>
 <?php
 	mysql_connect("127.0.0.1:3306", "root", "madmax1.") or die(mysql_error());
@@ -27,12 +29,25 @@ else if ($frame == 'internaldata')
 	{
 		
 		$selected_data_set = $_POST['data_set'];
+		$selected_ticker = $_POST['ticker'];
 		
-		$query1 = //"select data_set,ticker,value,static_url from view_fact_data,extract_info where view_fact_data.data_set=extract_info.Data_Set AND data_set='".$selected_data_set."'";
-		"SELECT view_fact_data.data_set, ticker, value, url_static
+		if (substr($selected_data_set,0,5) == "table")
+		{
+			$query1 = 	"SELECT view_fact_data.data_set, ticker, value, url_static, fiscalquarter, fiscalyear
+		FROM view_fact_data, extract_table
+		WHERE view_fact_data.data_set = extract_table.Data_Set
+		AND view_fact_data.data_set = '".$selected_data_set."' 
+		AND view_fact_data.ticker = '".$selected_ticker."'";
+		}
+		else
+		{
+		$query1 = 
+		"SELECT view_fact_data.data_set, ticker, value, url_static, fiscalquarter, fiscalyear
 		FROM view_fact_data, extract_info
 		WHERE view_fact_data.data_set = extract_info.Data_Set
-		AND view_fact_data.data_set = '".$selected_data_set."'";
+		AND view_fact_data.data_set = '".$selected_data_set."' 
+		AND view_fact_data.ticker = '".$selected_ticker."'";
+		}
 
 		$result1 = mysql_query($query1) or die("Failed Query of " . $query1);
 		
@@ -47,44 +62,19 @@ else if ($frame == 'internaldata')
 			echo "<TD>".$row1[data_set]."</TD>";
 			echo "<TD><A HREF=\"".$row1[url_static].$row1[ticker]."\" target=webpage>".$row1[ticker]."</A></TD>";
 			echo "<TD>".$row1[value]."</TD>";
+			echo "<TD>".$row1[fiscalyear]."</TD>";
+			echo "<TD>".$row1[fiscalquarter]."</TD>";
 			echo "</TR>";
 		}
 	
 	
 }
-else if(isset($_POST['submit_table_switch'])) 
-{
-		$query1 = "select count(primary_key) from view_fact_data";
-		$query2 = "select count(primary_key) from fact_data_stage";
-		
-		$result1 = mysql_query($query1) or die("Failed Query of " . $query1);
-		$row1 = mysql_fetch_array($result1);
-		$result2 = mysql_query($query2) or die("Failed Query of " . $query2);
-		$row2 = mysql_fetch_array($result2);
-		
-		//echo "<TR><TD>".$row1['count(primary_key)']."</TD>   <TD>".$row2['count(primary_key)']."</TD></TR>";
-		mysql_query("DROP VIEW view_fact_data") or die("Failed to drop view_fact_data");
-		
-		//previous view was based off of fact_data_stage, create new view off of fact_data
-		if ($row1['count(primary_key)'] == $row2['count(primary_key)'])
-		{
-				mysql_query("CREATE ALGORITHM = UNDEFINED VIEW `view_fact_data` AS SELECT * FROM `fact_data` ") or die("Failed to create view_fact_data");
-				echo "CREATED NEW VIEW FROM fact_data";
-		}
-		//create new view off of fact_data_stage
-		else
-		{
-			mysql_query("CREATE ALGORITHM = UNDEFINED VIEW `view_fact_data` AS SELECT * FROM `fact_data_stage` ") or die("Failed to create view_fact_data");
-			echo "CREATED NEW VIEW FROM fact_data_stage";
-		}
-		
-		
-		
-		
-}
+
 
 ?>
 </table>
+</body>
+</html>
 
 
 <?php
@@ -104,11 +94,69 @@ else if ($frame == 'form')
 	mysql_connect("127.0.0.1:3306", "root", "madmax1.") or die(mysql_error());
 	mysql_select_db("mydb") or die(mysql_error());
 ?>
-<table>
+<html>
+	<script type="text/javascript">
+	var companyArray;
+	function loadfunc()
+{
+	<?php
+	
+
+
+$sql="SELECT * FROM company order by ticker";
+
+$result = mysql_query($sql);
+
+echo "companyArray = new Array();";
+$i = 0;
+while($row = mysql_fetch_array($result))
+  {
+
+  echo "companyArray[".$i."]=\"".$row[ticker]."\";";
+  $i++;
+  }
+?>
+
+}
+function test1()
+{
+	
+	//var x=document.getElementById('company');
+	var z=document.getElementById('ticker');
+	
+	var a=document.getElementById('tables');
+	
+	var tmp = a.value.toUpperCase();
+	
+	for(var i=z.options.length-1;i>=0;i--)
+	{
+		z.remove(i);
+	}
+
+	
+	
+	for (var y=0;y<companyArray.length;y=y+1)
+	{
+		//z.add(x.options[y].text,x.options[y].text);
+		if (companyArray[y].indexOf(tmp) == 0)
+		{
+			var elOptNew = document.createElement('option');
+
+    	elOptNew.text = companyArray[y];
+    	elOptNew.value = companyArray[y];
+    	z.add(elOptNew);
+    }
+
+		//z.add("test","test");
+	}
+}
+</script>
+	<body onload="loadfunc();">
+<table border="1">
 <tr>
 	<td>
 <form name="SelectFactData" action="VerifyData.php?frame=internaldata" method=POST target=internaldata> 	
-					<table>
+					<table border="1">
 						<tr><td>
 							<!-- <select name="data_set" onchange="alert(this.value);" onchange="UpdateField(this.selectedIndex);"> -->
 							<select name="data_set">
@@ -127,45 +175,13 @@ else if ($frame == 'form')
 							</select>
 						</td>
 						<td>
-							<input type="submit" value="Submit" name="submit_internal" 
-							onclick="SelectFactData.action='VerifyData.php?frame=internaldata'; return true" >
-						</td></tr>
-							</table>
-							
-</form>
-</td>
-<td>
-	                                 
-	                               </td>
-<td>
-
-<form name="SelectFactData" action="VerifyData.php?frame=internaldata" method=POST target=internaldata> 	
-					<table>
-						<tr><td>
-<?php
-		$query1 = "select count(primary_key) from view_fact_data";
-		$query2 = "select count(primary_key) from fact_data_stage";
-		
-		$result1 = mysql_query($query1) or die("Failed Query of " . $query1);
-		$row1 = mysql_fetch_array($result1);
-		$result2 = mysql_query($query2) or die("Failed Query of " . $query2);
-		$row2 = mysql_fetch_array($result2);
-		
-		//echo "<TR><TD>".$row1['count(primary_key)']."</TD>   <TD>".$row2['count(primary_key)']."</TD></TR>";
-		
-		if ($row1['count(primary_key)'] == $row2['count(primary_key)']) 
-			echo "Current view: fact_data_stage";
-		else
-			echo "Current view: fact_data";
-			
-		
-	
-
-?>						
-						
+							<input type="text" name="tables" size=30 maxlength="30" onkeyup="test1()">
 						</td>
 						<td>
-							<input type="submit" value="Toggle Fact View" name="submit_table_switch" 
+							<select name="ticker" size=5></select>
+						</td>
+						<td>
+							<input type="submit" value="Submit" name="submit_internal" 
 							onclick="SelectFactData.action='VerifyData.php?frame=internaldata'; return true" >
 						</td></tr>
 							</table>
@@ -174,6 +190,8 @@ else if ($frame == 'form')
 </td>
 </tr>
 </table>
+</body>
+</html>
 
 <?php
 }
