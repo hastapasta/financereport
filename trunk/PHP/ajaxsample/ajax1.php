20be7b942022e446c0a788a21ea20b62c7c7252b
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-10-02 00:31:52 +0000

    Initial Load
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@125 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

---
+++ trunk/PHP/ajaxsample/ajax1.php
@@ -0,0 +1,44 @@
+<?php
+$q=$_GET["q"];
+
+//$con = mysql_connect('localhost', 'peter', 'abc123');
+$con = mysql_connect("127.0.0.1:3306", "root", "madmax1.") or die(mysql_error());
+mysql_select_db("mydb") or die(mysql_error());
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("mydb", $con);
+
+$sql="SELECT * FROM company WHERE ticker = '".$q."'";
+
+$result = mysql_query($sql);
+
+ echo "<BR> ticker: ".$q."<BR>";
+
+echo "<table border='1'>
+<tr>
+<th>company".$q."</th>
+<th>shares outstanding</th>
+<th>begin fiscal quarter</th>
+<th>groupssss</th>
+</tr>";
+
+
+ 
+
+while($row = mysql_fetch_array($result))
+  {
+
+  echo "<tr>";
+  echo "<td>" . $row['ticker'] . "</td>";
+  echo "<td>" . $row['shares_outstanding'] . "</td>";
+  echo "<td>" . $row['begin_fiscal_calendar'] . "</td>";
+  echo "<td>" . $row['groups'] . "</td>";
+  echo "</tr>";
+  }
+echo "</table>";
+
+mysql_close($con);
+?> 
---
+++ trunk/PHP/ajaxsample/picklist.php
@@ -0,0 +1,183 @@
+<html>
+<head>
+
+<script type="text/javascript">
+	var companyArray;
+	function loadfunc()
+{
+	<?php
+	
+
+	$con = mysql_connect("127.0.0.1:3306", "root", "madmax1.") or die(mysql_error());
+mysql_select_db("mydb") or die(mysql_error());
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("mydb", $con);
+
+$sql="SELECT * FROM company order by ticker";
+
+$result = mysql_query($sql);
+
+echo "companyArray = new Array();";
+$i = 0;
+while($row = mysql_fetch_array($result))
+  {
+
+  echo "companyArray[".$i."]=\"".$row[ticker]."\";";
+  $i++;
+  }
+?>
+
+}
+function showUser(str)
+{
+if (str=="")
+  {
+  document.getElementById("txtHint").innerHTML="";
+  return;
+  } 
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5  
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtCompany").innerHTML=xmlhttp.responseText;
+    }
+  }
+var currentTime = new Date()
+xmlhttp.open("POST","picklist1.php?q="+str+"&timestamp="+currentTime,true);
+xmlhttp.send();
+}
+
+function test()
+{
+
+if (window.XMLHttpRequest)
+  {// code for IE7+, Firefox, Chrome, Opera, Safari
+  xmlhttp=new XMLHttpRequest();
+  }
+else
+  {// code for IE6, IE5  
+  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+  }
+xmlhttp.onreadystatechange=function()
+  {
+  if (xmlhttp.readyState==4 && xmlhttp.status==200)
+    {
+    document.getElementById("txtDate").innerHTML=xmlhttp.responseText;
+    }
+  }
+var currentTime = new Date();
+xmlhttp.open("GET","picklist2.php?timestamp="+currentTime,true);
+xmlhttp.send();
+}
+
+function test1()
+{
+	
+	//var x=document.getElementById('company');
+	var z=document.getElementById('company2');
+	
+	var a=document.getElementById('tables');
+	
+	var tmp = a.value.toUpperCase();
+	
+	for(var i=z.options.length-1;i>=0;i--)
+	{
+		z.remove(i);
+	}
+
+	
+	
+	for (var y=0;y<companyArray.length;y=y+1)
+	{
+		//z.add(x.options[y].text,x.options[y].text);
+		if (companyArray[y].indexOf(tmp) == 0)
+		{
+			var elOptNew = document.createElement('option');
+
+    	elOptNew.text = companyArray[y];
+    	elOptNew.value = companyArray[y];
+    	z.add(elOptNew);
+    }
+
+		//z.add("test","test");
+	}
+}
+</script>
+</head>
+<body onLoad="loadfunc();">
+	<?php
+$con = mysql_connect("127.0.0.1:3306", "root", "madmax1.") or die(mysql_error());
+mysql_select_db("mydb") or die(mysql_error());
+if (!$con)
+  {
+  die('Could not connect: ' . mysql_error());
+  }
+
+mysql_select_db("mydb", $con);
+
+$sql="SELECT * FROM company order by ticker";
+
+$result = mysql_query($sql);
+
+/*//echo "<form><select name=\"company\" size=".mysql_num_rows($result)." onchange=\"test(\")>";
+echo "<form><select name=\"company\" size=10 onchange=\"test()\">";
+while($row = mysql_fetch_array($result))
+  {
+
+  echo "<option value=\"".$row[ticker]."\">".$row[ticker]."</option>";
+  }
+  
+ echo "</select></form>";*/
+ 
+
+
+
+
+?>
+
+Enter text in the text box to filter the list of companies:
+<form>
+<input type="text" name="tables" size=30 maxlength="30" onkeyup="test1()">
+</select>
+</form>
+
+<form><select name="company2" size=10 onchange="test()">
+</select>
+</form>
+
+<form>
+<select name="users" onchange="showUser(this.value)">
+<option value="">Select a ticker:</option>
+<option value="GE">GE</option>
+<option value="MSFT">MSFT</option>
+<option value="TRV">TRV</option>
+<option value="INTC">INTC</option>
+</select>
+</form>
+<div id="txtCompany"><b>Company info will be listed here.</b></div>
+<br>
+
+<br />
+
+
+Enter text in the text box to make an ajax call to get the current date/time:
+<form>
+<input type="text" name="tables" size=30 maxlength="30" onkeyup="test()">
+</select>
+</form>
+
+<div id="txtDate"><b>Date/time info will be listed here.</b></div>
+</body>
+</html> 
