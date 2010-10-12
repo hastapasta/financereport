24522c44f3cff4160b05d5a365ced9e8a70d4c8d
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-10-12 18:09:50 +0000

    Made the Connection property static. Added code to display a message if no database rows are updated since this condition is not otherwise indicated. Removed some lines that had been commented out for a while.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@155 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/DataLoad/UtilityFunctions.java
+++ trunk/Java/DataLoad/UtilityFunctions.java
@@ -15,7 +15,7 @@ import java.util.regex.*;
 public class UtilityFunctions
 {
 	
-	public Connection con;
+	public static Connection con;
 	static public boolean bCalledByJsp;
 	String strCapturedOutput;
 	static CustomBufferedWriter stdoutwriter;
@@ -63,7 +63,7 @@ public class UtilityFunctions
 		
 	}
 	
-	public void db_update_query(String strUpdateStmt) throws SQLException
+	public static void db_update_query(String strUpdateStmt) throws SQLException
 	{
 
 	
@@ -74,6 +74,9 @@ public class UtilityFunctions
 			stdoutwriter.writeln(strUpdateStmt,Logs.SQL,"UF2");
 			Statement stmt = con.createStatement();
 			stmt.execute(strUpdateStmt);
+			if (stmt.getUpdateCount() == 0)
+				stdoutwriter.writeln("No rows were updated",Logs.ERROR,"UF2.5");
+				
 			
 		/*}
 		catch (SQLException sqle)
@@ -93,7 +96,7 @@ public class UtilityFunctions
 	
 	
 	
-	public ResultSet db_run_query(String query) throws SQLException
+	public static ResultSet db_run_query(String query) throws SQLException
 	{
 
 		ResultSet rs =null;
@@ -101,7 +104,7 @@ public class UtilityFunctions
 		//{
 
 			stdoutwriter.writeln("Executing query: " + query, Logs.SQL,"UF3");
-			Statement stmt = this.con.createStatement();
+			Statement stmt = con.createStatement();
 			stdoutwriter.writeln(query, Logs.SQL,"UF4");
 			rs = stmt.executeQuery(query);
 
@@ -203,7 +206,8 @@ public class UtilityFunctions
 			if (tablename.equals("fact_data_stage"))
 			{
 					rowdata = extendArray(rowdata);
-					rowdata[rowdata.length - 1] = Integer.toString(DataGrab.nBatch);
+					//using columnnames here since that is guaranteed to be the correct length
+					rowdata[columnnames.length - 1] = Integer.toString(DataGrab.nBatch);
 					
 			}
 			
@@ -512,43 +516,7 @@ public class UtilityFunctions
 		}
 	}
 	
-	public int retrieveAdjustedQuarter(int fiscalquarter,int fiscalyear, String strTicker)
-	{
-
-		String query="select begin_fiscal_calendar, company.ticker,";
-	    query = query + " (case when begin_fiscal_calendar in ('February','March','April') then 3";
-	    query = query + " when begin_fiscal_calendar in ('May','June','July') then 2 when";
-	    query = query + " begin_fiscal_calendar in ('August','September','October') then 1";
-	    query = query + " else 0 end) as qtr_code_adjusted from company where";
-	    query = query + " company.ticker='" + strTicker + "'";
-		
-		/*String temp = strDataSet.substring(strDataSet.indexOf("_")+1, strDataSet.indexOf("_")+5);
-		Integer quarter = Integer.parseInt(temp.substring(1,2));
-		Integer year = Integer.parseInt(temp.substring(2,4));*/
-		Integer unadjustedqtr = (4*fiscalyear) + fiscalquarter;
-		Integer adjustment_code = 0;
-		
-		try
-		{
-			ResultSet rs = db_run_query(query);
-			rs.next();
-			adjustment_code = rs.getInt("qtr_code_adjusted");
-			
-		}
-		catch (SQLException sqle)
-		{
-			UtilityFunctions.stdoutwriter.writeln("Problem adjusting quarter value",Logs.ERROR,"UF24");
-			UtilityFunctions.stdoutwriter.writeln(sqle);
-			
-		}
-		
-		return(unadjustedqtr-adjustment_code);
-	    
-	 
-	    
-		
-		
-	}
+	
 	
 	public static void scrubCSV(String strFilename, String strDelimiter, String strEnclosure, String strOutFilename)
 	{
@@ -666,91 +634,7 @@ public class UtilityFunctions
 		
 	}
 	
-	public String getFiscalYearAndQuarter(String strTicker, int curmonth, int curyear) throws SQLException
-	{
-		/*NOTE: If a 2 digit year is submitted, a 2 digit year is returned. 
-		 *  4 digit year submitted, 4 digits returned.
-		 * 
-		 * Need to do a calculation to figure out the current fiscal quarter
-		 * 
-		 * cur month = 10, begin month = 1, then cur fiscal quarter is 4, fiscal year same as calendar year
-		 * cur month = 5, begin month = 6, the cur quarter is 4, 
-		 * cur month = 1, begin month = 6 then cur quarter is 3
-		 * cur month = 1, begin month = 12
-		 * cur month = 10, begin month = 5, the cur fiscal quarter is 2
-		 * 
-		 * (cur month - begin month) = x
-		 * if x>0   (x div 3) + 1
-		 * if x<0   ((12 + x) div 3) + 1
-		 * 
-		 * calculation for current year
-		 * 
-		 * begin fiscal calendar = 1 (boundary condition) fiscal year always = current year
-		 * 
-		 * other wise if current fiscal quarter <= current calendar quarter fiscal year = next year
-		 * else
-		 * current year 
-		 * 
-		 */
-		//try
-		//{
-			Integer nBeginFiscalYear=0;
-			String query = "select begin_fiscal_calendar from company where ticker='" + strTicker +"'";
-			ResultSet rs = db_run_query(query);
-			rs.next();
-			String strBeginFiscalYear = rs.getString("begin_fiscal_calendar");
-			Calendar cal = Calendar.getInstance(); 
-			if (curmonth == -1)
-			/* if curmonth = -1, get month and year from current data, otherwise use parameter values */
-			{
-				curmonth = cal.get(Calendar.MONTH) + 1;      
-				curyear = cal.get(Calendar.YEAR);
-			}
-			if (strBeginFiscalYear.equals("January")) nBeginFiscalYear = 1;
-			else if (strBeginFiscalYear.equals("February")) nBeginFiscalYear = 2;
-			else if (strBeginFiscalYear.equals("March")) nBeginFiscalYear = 3;
-			else if (strBeginFiscalYear.equals("April")) nBeginFiscalYear = 4;
-			else if (strBeginFiscalYear.equals("May")) nBeginFiscalYear = 5;
-			else if (strBeginFiscalYear.equals("June")) nBeginFiscalYear = 6;
-			else if (strBeginFiscalYear.equals("July")) nBeginFiscalYear = 7;
-			else if (strBeginFiscalYear.equals("August")) nBeginFiscalYear = 8;
-			else if (strBeginFiscalYear.equals("September")) nBeginFiscalYear = 9;
-			else if (strBeginFiscalYear.equals("October")) nBeginFiscalYear = 10;
-			else if (strBeginFiscalYear.equals("November")) nBeginFiscalYear = 11;
-			else if (strBeginFiscalYear.equals("December")) nBeginFiscalYear = 12;
-			else stdoutwriter.writeln("ERROR: Month not converted to int.",Logs.ERROR,"UF29");
-			Integer nCurFiscalQuarter, nCurFiscalYear;
-			if ((curmonth - nBeginFiscalYear)>=0)
-				nCurFiscalQuarter = ((curmonth - nBeginFiscalYear) / 3) + 1;
-			else
-				nCurFiscalQuarter = ((12 + (curmonth - nBeginFiscalYear)) / 3) + 1;
-			
-			if (nBeginFiscalYear == 1)
-			//Boundary condition if begin fiscal year is january, fiscal year is always the same as the current year
-				nCurFiscalYear = curyear;
-			else if ((curmonth - nBeginFiscalYear) >= 0)
-				nCurFiscalYear = curyear + 1;
-			else
-				nCurFiscalYear = curyear;
-			
-			String retval = Integer.toString(nCurFiscalQuarter) + Integer.toString(nCurFiscalYear);
-			return(retval);
-			
-			
-			
-		//}
-		/*catch (SQLException sqle)
-		{
-			stdoutwriter.writeln("Problem with query",Logs.ERROR);
-			stdoutwriter.writeln(sqle);
-			return("000000");
-		}*/
-		
-		
-		
-		
-		
-	}
+	
 	
 	public String[] extendArray(String[] inputArray)
 	{
@@ -762,6 +646,8 @@ public class UtilityFunctions
 		return(tmpArray);
 		
 	}
+	
+	
 }
 	
 	class TestException extends Exception
@@ -773,6 +659,8 @@ public class UtilityFunctions
 		
 		
 	}
+	
+	
 	
 	
 
