686e7260bcab243a5cc48a3eb97398756cc2fd9a
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-10-12 04:44:39 +0000

    Adjusted the classpath for the apache libraries
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@149 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/DataLoad/scripts/run.bat
+++ trunk/Java/DataLoad/scripts/run.bat
@@ -1,4 +1,4 @@
 set javahome=C:\progra~1\java\jdk1.6.0_18
-set classpath=.;C:\dev\java\ServiceTest;%javahome%;C:\dev\java\ServiceTest\com\roeschter\jsl;C:\progra~1\MySQL\mysql-connector-java-5.1.12-bin.jar
+set classpath=.;C:\dev\java\ServiceTest;%javahome%;C:\dev\java\ServiceTest\com\roeschter\jsl;C:\progra~1\MySQL\mysql-connector-java-5.1.12-bin.jar;C:\dev\java\libraries\commons-logging-1.1.1\commons-logging-1.1.1.jar;C:\dev\java\libraries\httpcomponents-client-4.0.3\lib\httpclient-4.0.3.jar;C:\dev\java\libraries\httpcomponents-core-4.0.1\lib\httpcore-4.0.1.jar
 set PATH=%PATH%;%javahome%\bin
 java -classpath "%classpath%" -Dservice.inifile=C:\dev\java\ServiceTest\jsl.ini com.roeschter.jsl.DataLoad 2>error.log
