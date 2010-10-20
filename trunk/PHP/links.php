b2f8137fcbc4240743f7f3114d14de0e6de34b53
admin <admin@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2010-10-20 18:16:11 +0000

    Made link references relative. Added editview.php link.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@158 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/links.php
+++ trunk/PHP/links.php
@@ -1,23 +1,24 @@
 <HTML>
 	<BODY>
-<A HREF="http://win-d2sjsg6emdd/dev/regex_find_offset.php">regex_find_offset</A><BR>
+<A HREF="regex_find_offset.php">regex_find_offset</A><BR>
 
-<A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=URL_Initial_UE_Claims&insert=1">data_grab_v2?Data_Set=URL_Initial_UE_Claims&insert=1</A><BR>
-<A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=yahoo_ge_qtr_net_income&insert=1">data_grab_v2?Data_Set=yahoo_ge_qtr_net_income&insert=1</A><BR>
-<A HREF="http://win-d2sjsg6emdd/dev/data_grab_v2.php?Data_Set=yahoo_shares_outstand&insert=0">data_grab_v2?Data_Set=yahoo_shares_outstand&insert=1</A><BR>
-<A HREF="http://win-d2sjsg6emdd/dev/InitialLoad.php">Initial Load</A><BR>
-<A HREF="http://win-d2sjsg6emdd/dev/DisplayDataSet.php">Display Data Set</A><BR>
-<A HREF="http://win-d2sjsg6emdd/dev/VerifyData.php?frame=parent">Verify Data</A><BR>
-<A HREF="http://win-d2sjsg6emdd/dev/toggleview.php">Toggle fact view</A><BR>
+<A HREF="data_grab_v2.php?Data_Set=URL_Initial_UE_Claims&insert=1">data_grab_v2?Data_Set=URL_Initial_UE_Claims&insert=1</A><BR>
+<A HREF="data_grab_v2.php?Data_Set=yahoo_ge_qtr_net_income&insert=1">data_grab_v2?Data_Set=yahoo_ge_qtr_net_income&insert=1</A><BR>
+<A HREF="data_grab_v2.php?Data_Set=yahoo_shares_outstand&insert=0">data_grab_v2?Data_Set=yahoo_shares_outstand&insert=1</A><BR>
+<A HREF="InitialLoad.php">Initial Load</A><BR>
+<A HREF="DisplayDataSet.php">Display Data Set</A><BR>
+<A HREF="VerifyData.php?frame=parent">Verify Data</A><BR>
+<A HREF="toggleview.php">Toggle fact view</A><BR>
+<A HREF="editview.php">Edit MYSQL views</A><BR>
 <BR>
-<A HREF="http://win-d2sjsg6emdd/phpmyadmin">phpmyadmin</A><BR>
+<A HREF="../phpmyadmin">phpmyadmin</A><BR>
 <BR>
-<A HREF="http://win-d2sjsg6emdd/phpjobscheduler/index.php">Job Scheduler</A><BR>
-<A HREF="http://win-d2sjsg6emdd/phpjobscheduler/pjsfiles/index.php">pjsfiles/index.php</A><BR>
-<A HREF="http://win-d2sjsg6emdd/phpjobscheduler/pjsfiles/phpjobscheduler.php">fire php jobs</A><BR>
+<A HREF="../phpjobscheduler/index.php">Job Scheduler</A><BR>
+<A HREF="../phpjobscheduler/pjsfiles/index.php">pjsfiles/index.php</A><BR>
+<A HREF="../phpjobscheduler/pjsfiles/phpjobscheduler.php">fire php jobs</A><BR>
 <BR>
-<A HREF="http://win-d2sjsg6emdd/examples/jsp/checkbox/controlpanel.jsp?frame=parent">Control Panel</A><BR>
-<A HREF="http://win-d2sjsg6emdd/examples/jsp/checkbox/check.html">Checkbox jsp sample</A><BR>
+<A HREF="../examples/jsp/checkbox/controlpanel.jsp?frame=parent">Control Panel</A><BR>
+<A HREF="../examples/jsp/checkbox/check.html">Checkbox jsp sample</A><BR>
 <BR>
 <A HREF="http://win-d2sjsg6emdd:8080/manager/html">Tomcat Manager</A><BR>
 
