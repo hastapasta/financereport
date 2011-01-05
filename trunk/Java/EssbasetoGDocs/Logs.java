5a7b8df8d0aee4760ec2504c7616af64badbc354
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-01-05 19:14:21 +0000

    Initial import.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@240 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

---
+++ trunk/Java/EssbasetoGDocs/Logs.java
@@ -0,0 +1,9 @@
+//package com.roeschter.jsl;
+
+/* 
+ * STATUS1 messages are high level and also get written to System.out.println
+ * STATUS2 are lower level, more detailed messages and are only written to the log file.
+ */
+public enum Logs {FULL,ERROR,SQL,THREAD,STATUS1,STATUS2,NONE} 
+
+
