f7c2f40e4d33d3964bfcc8c3c8cae667dd226f43
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-01-11 19:32:30 +0000

    Added enhanced error handling to the progress handler. Now an alert dialog box is displayed.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@285 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/TestTabs3/src/com/example/android/apis/view/EarningsActivity.java
+++ trunk/Java/TestTabs3/src/com/example/android/apis/view/EarningsActivity.java
@@ -1,28 +1,39 @@
 package com.example.android.apis.view;
 
+import java.util.Map;
+
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.example.android.apis.R;
 
+
 import android.app.Activity;
 import android.app.AlertDialog;
 //import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 //import android.view.Window;
 //import android.widget.Button;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.TableRow.LayoutParams;
 
 
@@ -39,6 +50,12 @@ public class EarningsActivity extends Activity implements View.OnClickListener{
 	int nRowCount;
 	String[] fieldsRow1;
 	String[] fieldsRow2;
+	String strErrorMessage;
+	
+	   /**
+     * Safe to hold on to this.
+     */
+    private Menu mMenu;
 	
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -55,6 +72,8 @@ public class EarningsActivity extends Activity implements View.OnClickListener{
         
         
         strType = "weaker";
+        
+        //addPreferencesFromResource(R.xml.default_values1);
         
       /*  View loginButton = findViewById(R.id.button_id);
        
@@ -87,9 +106,77 @@ public class EarningsActivity extends Activity implements View.OnClickListener{
         setContentView(textview);*/
     }
     
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Hold on to this
+        mMenu = menu;
+        
+        // Inflate the currently selected menu XML resource.
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.title_only, menu);
+        //inflater.inflate(sMenuExampleResources[mSpinner.getSelectedItemPosition()], menu);
+        
+        // Disable the spinner since we've already created the menu and the user
+        // can no longer pick a different menu XML.
+        //mSpinner.setEnabled(false);
+        
+        // Change instructions
+        /*mInstructionsText.setText(getResources().getString(
+                R.string.menu_from_xml_instructions_go_back));*/
+        
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            // For "Title only": Examples of matching an ID with one assigned in
+            //                   the XML
+            case R.id.settings:
+                //Toast.makeText(this, "Jump up in the air!", Toast.LENGTH_SHORT).show();
+            	Intent intent = new Intent(EarningsActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.dive:
+                Toast.makeText(this, "Dive into the water!", Toast.LENGTH_SHORT).show();
+                return true;
+
+            // For "Groups": Toggle visibility of grouped menu items with
+            //               nongrouped menu items
+            case R.id.browser_visibility:
+                // The refresh item is part of the browser group
+                final boolean shouldShowBrowser = !mMenu.findItem(R.id.refresh).isVisible();
+                mMenu.setGroupVisible(R.id.browser, shouldShowBrowser);
+                break;
+                
+            case R.id.email_visibility:
+                // The reply item is part of the email group
+                final boolean shouldShowEmail = !mMenu.findItem(R.id.reply).isVisible();
+                mMenu.setGroupVisible(R.id.email, shouldShowEmail);
+                break;
+                
+            // Generic catch all for all the other menu resources
+            default:
+                // Don't toast text when a submenu is clicked
+                if (!item.hasSubMenu()) {
+                    Toast.makeText(this, item.getTitle(), Toast.LENGTH_SHORT).show();
+                    return true;
+                }
+                break;
+        }
+        
+        return false;
+    }
+    
     public void onResume()
     {
     	super.onResume();
+    	
+    	SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(this);
+    	
+    	String tmp = sp.getString("json_url_pref","-1");
+    	
     	mDialog2 = ProgressDialog.show(this, "", "Updating...", true, false);
     	//tl.removeViews(1, nRowCount);
     	if (tl.getChildCount() > 0)
@@ -130,6 +217,8 @@ public class EarningsActivity extends Activity implements View.OnClickListener{
 
      
     }
+    
+    
     
     public void populate2(Activity tmpActivity,TableLayout tl,String[] fieldNames,String[] fieldNames2,JSONArray jArray)
     {
@@ -275,7 +364,7 @@ public class EarningsActivity extends Activity implements View.OnClickListener{
                         case 1:
                         	alertDialog = new AlertDialog.Builder(EarningsActivity.this).create();
                             alertDialog.setTitle("Alert");
-                            alertDialog.setMessage("Unable to retrieve data.");
+                            alertDialog.setMessage(strErrorMessage);
                             alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
                               public void onClick(DialogInterface dialog, int which) {
                                 return;
@@ -293,7 +382,9 @@ public class EarningsActivity extends Activity implements View.OnClickListener{
                 };
     
     
-    
+    /*
+     * This is the class for the wait/progress dialog.
+     */
     private class SearchThread extends Thread {
 
         //private Search search;
@@ -312,18 +403,34 @@ public class EarningsActivity extends Activity implements View.OnClickListener{
         	//retrieve(tl);
         	try
         	{
-        		EarningsActivity.this.jArray = MySQLConnect.connect(EarningsActivity.this.getString(R.string.json_url) + "pebymarketcap.php",strType);
+        		SharedPreferences sp = PreferenceManager.getDefaultSharedPreferences(EarningsActivity.this);
+        		
+       
+        		Map tmp1 = sp.getAll();
+        	
+        		String tmp = sp.getString("json_url_pref","-1");
+        		
+        		//EarningsActivity.this.jArray = MySQLConnect.connect(EarningsActivity.this.getString(R.string.json_url) + "pebymarketcap.php",strType);
+        		EarningsActivity.this.jArray = MySQLConnect.connect(tmp + "pebymarketcap.php",strType);
         		handler.sendEmptyMessage(0);
         	}
         	catch (CustomConnectionException cce)
         	{
+        		/*
+        		 * Basically using a global variable. I'd prefer to do this a different way but not sure how right now.
+        		 */
+        		EarningsActivity.this.strErrorMessage = cce.getMessage();
         		handler.sendEmptyMessage(1);
+      
         	}
         	
 
            
         }
 
+       /*
+        * Handler for the progress dialog. 
+        */
        private Handler handler = new Handler() {
 
             @Override
@@ -331,8 +438,33 @@ public class EarningsActivity extends Activity implements View.OnClickListener{
             {
             
                 //displaySearchResults();
+            	/*
+            	 * Remove the progress dialog.
+            	 */
               	mDialog2.dismiss();
-              	handler2.sendEmptyMessage(0);
+              	switch(msg.what){
+                case 0:
+                       /*
+                        * Success. Go display the data.
+                        */
+                		handler2.sendEmptyMessage(0);
+                       break;
+                case 1:
+                	handler2.sendEmptyMessage(1);
+                	/*alertDialog = new AlertDialog.Builder(EarningsActivity.this).create();
+                    alertDialog.setTitle("Alert");
+                    alertDialog.setMessage("Unable to retrieve data.");
+                    alertDialog.setButton("OK", new DialogInterface.OnClickListener() {
+                      public void onClick(DialogInterface dialog, int which) {
+                        return;
+                      }
+                    });
+                    alertDialog.show();*/
+                	
+                	break;
+              }
+              	
+             
             }
             
         };
