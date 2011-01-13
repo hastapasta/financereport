02ae214d53ae03ccf2d05faf61089bbd834251b4
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-01-13 19:23:59 +0000

    Updated and organized the links.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@303 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/common/links.php
+++ trunk/PHP/common/links.php
@@ -1,31 +1,34 @@
 <HTML>
 	<BODY>
-<A HREF="regex_find_offset.php">regex_find_offset</A><BR>
+<A HREF="../find_offset_insert_data/regex_find_offset.php">regex_find_offset</A><BR>
 
-<A HREF="data_grab_v2.php?Data_Set=URL_Initial_UE_Claims&insert=1">data_grab_v2?Data_Set=URL_Initial_UE_Claims&insert=1</A><BR>
-<A HREF="data_grab_v2.php?Data_Set=yahoo_ge_qtr_net_income&insert=1">data_grab_v2?Data_Set=yahoo_ge_qtr_net_income&insert=1</A><BR>
-<A HREF="data_grab_v2.php?Data_Set=yahoo_shares_outstand&insert=0">data_grab_v2?Data_Set=yahoo_shares_outstand&insert=1</A><BR>
-<A HREF="InitialLoad.php">Initial Load</A><BR>
-<A HREF="DisplayDataSet.php">Display Data Set</A><BR>
-<A HREF="VerifyData.php?frame=parent">Verify Data</A><BR>
-<A HREF="toggleview.php">Toggle fact view</A><BR>
-<A HREF="editview.php">Edit MYSQL views</A><BR>
+
+<A HREF="../misc_dataload_php/DisplayDataSet.php">Display Data Set</A><BR>
+<A HREF="../misc_dataload_php/VerifyData.php?frame=parent">Verify Data</A><BR>
+<A HREF="../misc_dataload_php/toggleview.php">Toggle fact view</A><BR>
+<A HREF="../misc_dataload_php/editview.php">Edit MYSQL views</A><BR>
+
+<A HREF="../misc_dataload_php/data_grab_v2.php?Data_Set=URL_Initial_UE_Claims&insert=1">data_grab_v2?Data_Set=URL_Initial_UE_Claims&insert=1</A><BR>
+<A HREF="../misc_dataload_php/data_grab_v2.php?Data_Set=yahoo_ge_qtr_net_income&insert=1">data_grab_v2?Data_Set=yahoo_ge_qtr_net_income&insert=1</A><BR>
+<A HREF="../misc_dataload_php/data_grab_v2.php?Data_Set=yahoo_shares_outstand&insert=0">data_grab_v2?Data_Set=yahoo_shares_outstand&insert=1</A><BR>
 <BR>
+Data Grid Links<BR>
+<A HREF="../../notifydatagrid/editnotification.php">Edit Notification</A>
+<BR><BR>
 <A HREF="../phpmyadmin">phpmyadmin</A><BR>
 <BR>
-<A HREF="../phpjobscheduler/index.php">Job Scheduler</A><BR>
-<A HREF="../phpjobscheduler/pjsfiles/index.php">pjsfiles/index.php</A><BR>
-<A HREF="../phpjobscheduler/pjsfiles/phpjobscheduler.php">fire php jobs</A><BR>
-<BR>
+JSP Links<BR>
 <A HREF="../examples/jsp/checkbox/controlpanel.jsp?frame=parent">Control Panel</A><BR>
 <A HREF="../examples/jsp/checkbox/check.html">Checkbox jsp sample</A><BR>
 <BR>
 <A HREF="http://win-d2sjsg6emdd:8080/manager/html">Tomcat Manager</A><BR>
-<BR>
+<BR>Oracle EPM Links<BR>
 <A HREF="http://oracleepm:19000/workspace">Workspace</A><BR>
 <A HREF="http://oracleepm:10080/easconsole/console.html">EAS Console</A><BR>
 <A HREF="http://oracleepm:55000/cmc/index.jsp">Configuration Console</A><BR>
 <A HREF="http://oracleepm:16000/WebAnalysis/WebAnalysis.jsp">WebAnalysis</A><BR>
+<BR>
+<A HREF="http://www.pikefin.com/pandora_console/index.php">Pandora Console</A><BR>
 
 
 
