9c4c7d95ab26b6d7b5750317ad1e1ddace176d10
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-01-13 20:13:05 +0000

    Various changes...still doing intial development.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@304 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/misc_dataload_php/editalert.php
+++ trunk/PHP/misc_dataload_php/editalert.php
@@ -6,9 +6,9 @@
 ?>
 <HTML>
 	<BODY>
-	INCREASE ALERT LIMIT
+	
 				
-		<table>
+
 
 <?php
 
@@ -21,36 +21,35 @@
 					die("No pkey parameter set. Terminating.");
 				}
 				
+				if (isset($_GET['currentvalue']))
+				{
+					$currentvalue = $_GET['currentvalue'];
+				}
+				
+			
 				if(isset($_POST['submit_msg'])) 
 				{
-					$selected_data_set = $_POST['data_set'];
 					
-				$query1 = "select data_set,ticker,value from fact_data where data_set='".$selected_data_set."'";
+					
+				$query1 = "update notify set limit_adjustment=".$_POST['limit_increase']." where primary_key=".$primary_key;
 				
-				$result1 = mysql_query($query1) or die("Failed Query of " . $query1);
 				
-				for ($j=0;$j<mysql_num_rows($result1);$j++)
-				//for ($j=0;$j<2;$j++)
-				{
-					
-					$row1 = mysql_fetch_array($result1);
-					
-					echo "<TR>";
-					echo "<TD>".$row1[data_set]."</TD>";
-					echo "<TD>".$row1[ticker]."</TD>";
-					echo "<TD>".$row1[value]."</TD>";
-					echo "</TR>";
-					
-					
-					
-					
-				}
-			}
+				if (mysql_query($query1))
+  					echo "Limit Value Updated.<BR>";  			
+  				else
+  	 				die(mysql_error());
+  	 				}
+  	 	?>
+					</table>
+				
 			
 
-?>
-				</table>
-				<form action="DisplayDataSet.php" method=POST > 
+INCREASE ALERT LIMIT
+			
+				<?php 
+				echo "<form action=\"editalert.php?pkey=".$primary_key."&currentvalue=".$currentvalue."\" method=POST > ";
+				?>
+				
 					<table>
 						
 							
@@ -62,24 +61,37 @@
 									$row2 = mysql_fetch_array($result2);
 									//<input type=\"text\" name=\"".$field."\" size=".$size." maxlength=\"".$length."\" >
 									//echo "<TR><TD>Type: </TD><TD><input type=\"text\" name=\"type\" size=20 value=\"".$row2['type']."\" ></TD></TR>";
-									echo "<TR><TD>Data Set: </TD><TD>".$row2['data_set']."</TD></TR>";
-									echo "<TR><TD>Email: </TD><TD>".$row2['email']."</TD></TR>";	
-									echo "<TR><TD>Begin Value: </TD><TD>".$row2['value']."</TD></TR>";
-									echo "<TR><TD>Current Value: </TD><TD></TD></TR>";
-									echo "<TR><TD>Frequency: </TD><TD>".$row2['frequency']."</TD></TR>";
-									echo "<TR><TD>Begin Time: </TD><TD>".$row2['date_collected']."</TD></TR>";
 									if (!empty($row2['ticker']))
 									{
 										echo "<TR><TD>Ticker: </TD><TD>".$row2['ticker']."</TD></TR>";
-										$query3 = "select description from company where ticker='".$row['ticker']."'";
+										$query3 = "select full_name from company where ticker='".$row2['ticker']."'";
 										$result3 = mysql_query($query3) or die("Failed Query of " . $query3);
 										$row3 = mysql_fetch_array($result3);
 										
-										echo "<TR><TD>Ticker Description: </TD><TD>".$row3['description']."</TD></TR>";
+										echo "<TR><TD>Ticker Description: </TD><TD>".$row3['full_name']."</TD></TR>";
 										
 									}
-									echo "<TR><TD>Limit Value: </TD><TD>".$row2['limit_value']."</TD></TR>";
-									echo "<TR><TD>Limit Increase: </TD><TD><input type=\"text\" name=\"type\" size=20 value=\"".$row2['limit_adjustment']."\" ></TD></TR>";
+									echo "<TR><TD>Data Set: </TD><TD>".$row2['data_set']."</TD></TR>";
+									echo "<TR><TD>Email: </TD><TD>".$row2['email']."</TD></TR>";
+
+									$beginvalue = $row2['value'];
+									
+									echo "<TR><TD>Begin Value: </TD><TD>".$beginvalue."</TD></TR>";
+									echo "<TR><TD>Current Value: </TD><TD>".$currentvalue."</TD></TR>";
+									
+									$percentdiff = round((($currentvalue - $beginvalue)/$beginvalue) * 100,2);
+									
+									echo "<TR><TD>Percent Difference: </TD><TD>".$percentdiff."%</TD></TR>";
+									
+									
+									echo "<TR><TD>Frequency: </TD><TD>".$row2['frequency']."</TD></TR>";
+									echo "<TR><TD>Begin Time: </TD><TD>".$row2['date_collected']."</TD></TR>";
+									
+							
+									echo "<TR><TD>Base Limit Value: </TD><TD>".$row2['limit_value']."</TD></TR>";
+									echo "<TR><TD>Limit Value Adjustment: </TD><TD>".$row2['limit_adjustment']."</TD></TR>";
+									
+									echo "<TR><TD>Increase Limit Value Adjustment To: </TD><TD><input type=\"text\" name=\"limit_increase\" size=20 value=\"".$row2['limit_adjustment']."\" ></TD></TR>";
 									
 								
 								?>
@@ -88,7 +100,12 @@
 					</table>
 				</form>
 
+				
+				
 	
+			
+
+		
 	
 	</BODY>
 </HTML>
