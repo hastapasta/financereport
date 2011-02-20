0bad41d71afe4670f807de0087de1ea584efa94f
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-02-20 22:12:38 +0000

    -indian guys added code for multi edit/delete. -commented out intiDB() function
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@345 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/cakepf/controllers/users_controller.php
+++ trunk/PHP/cakepf/controllers/users_controller.php
@@ -5,16 +5,17 @@ class UsersController extends AppController {
 
 	function beforeFilter() {
 		/*
-		* Production setup.
-		*/
-	  	parent::beforeFilter(); 
+		 * Production setup.
+		 */
+		parent::beforeFilter();
+
 		$this->Auth->allow('logout');
 
 		/*
-		* End Production setup.
-		*/
+		 * End Production setup.
+		 */
 
-	    //$this->Auth->allow(array('*'));
+		//$this->Auth->allow(array('*'));
 	}
 
 
@@ -46,20 +47,27 @@ class UsersController extends AppController {
 	}
 
 	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid user', true));
-			$this->redirect(array('action' => 'index'));
-		}
+		$record = $this->Session->read('Record');
 		if (!empty($this->data)) {
-			if ($this->User->save($this->data)) {
+			for($i = 0; $i < sizeof($this->data['User']); $i++){
+				$this->data['User'][$i]['password'] = $this->Auth->password($this->data['User'][$i]['password']);
+			}
+			if ($this->User->saveAll($this->data['User'])) {
 				$this->Session->setFlash(__('The user has been saved', true));
+				$this->Session->delete('Record');
 				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
 			}
 		}
+		if (!empty($record)) {
+			//$this->data = $this->User->read(null, $id);
+			$this->data = $this->toMulti($this->User->find('all',array('conditions'=>array('User.id'=>$record))));
+				
+		}
 		if (empty($this->data)) {
-			$this->data = $this->User->read(null, $id);
+			$this->Session->setFlash(__('Invalid user', true));
+			$this->redirect(array('action' => 'index'));
 		}
 		$groups = $this->User->Group->find('list');
 		$this->set(compact('groups'));
@@ -81,37 +89,38 @@ class UsersController extends AppController {
 	function login() {
 		if ($this->Session->read('Auth.User')) {
 			$this->Session->setFlash('You are logged in!');
-			$this->redirect('/alerts', null, false);
+				
+			//$this->redirect('/alerts', null, false);
 		}
-	}   
-	 
+	}
+
 	function logout() {
-	   	$this->Session->setFlash('Good-Bye');
+		$this->Session->setFlash('Good-Bye');
 		$this->redirect($this->Auth->logout());
 	}
 
-	function initDB() {
-	    $group =& $this->User->Group;
-	    //Allow admins to everything
-	    $group->id = 1;     
-	    $this->Acl->allow($group, 'controllers');
-	 
-	    //allow managers to posts and widgets
-	    $group->id = 2;
-	    $this->Acl->deny($group, 'controllers');
-	    $this->Acl->allow($group, 'controllers/Alerts');
-	    $this->Acl->allow($group, 'controllers/Schedules');
-	 
-	    //allow users to only add and edit on posts and widgets
-	    $group->id = 3;
-	    $this->Acl->deny($group, 'controllers');        
-	    $this->Acl->allow($group, 'controllers/Alerts/add');
-	    $this->Acl->allow($group, 'controllers/Alerts/edit');        
-	
-	    //we add an exit to avoid an ugly "missing views" error message
-	    echo "all done";
-	    exit;
-	}
+	/*function initDB() {
+		$group =& $this->User->Group;
+		//Allow admins to everything
+		$group->id = 1;
+		$this->Acl->allow($group, 'controllers');
+
+		//allow managers to posts and widgets
+		$group->id = 2;
+		$this->Acl->deny($group, 'controllers');
+		$this->Acl->allow($group, 'controllers/Alerts');
+		$this->Acl->allow($group, 'controllers/Schedules');
+
+		//allow users to only add and edit on posts and widgets
+		$group->id = 3;
+		$this->Acl->deny($group, 'controllers');
+		$this->Acl->allow($group, 'controllers/Alerts');
+		//$this->Acl->allow($group, 'controllers/Alerts/edit');
+
+		//we add an exit to avoid an ugly "missing views" error message
+		echo "all done";
+		exit;
+	}*/
 
 
 }
