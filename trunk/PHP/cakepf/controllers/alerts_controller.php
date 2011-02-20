a9753c9bf67f2648cecb8c48775198d70444e3c5
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-02-20 21:36:57 +0000

    Many, many changes. -functionaltiy for multi edit/delete -fixed the filter controls -adjusted filtering by user
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@336 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/cakepf/controllers/alerts_controller.php
+++ trunk/PHP/cakepf/controllers/alerts_controller.php
@@ -2,49 +2,133 @@
 class AlertsController extends AppController {
 
 	var $name = 'Alerts';
-	var $uses = array('Alert','Schedule','Task', 'User','Entity');
+	var $uses = array('Alert','Schedule','Task', 'User','EntityGroup','Entity');
 
 	function beforeFilter() {
-	    parent::beforeFilter(); 
-	    $this->Auth->allowedActions = array('index', 'view');
+		$this->log('in alerts_controller beforeFilter', LOG_DEBUG);
+		parent::beforeFilter();
+		
+
+		
+		//$this->log(debug_backtrace(), LOG_DEBUG);
+	  
+		//$this->log($this,LOG_DEBUG);
+	
+	  
+	  
+	  
+		//$this->set('urlrequest',$this->referer());
+		//$this->Auth->allowedActions = array('index', 'view');
 
 		/*
-		* Technically we should be pulling a list of all tasks that are referenced in the schedules table, but
-		* for now we'll just pull a list straight from the tasks table.
-		*/
+		 * Technically we should be pulling a list of all tasks that are referenced in the schedules table, but
+		 * for now we'll just pull a list straight from the tasks table.
+		 */
 
-		//App::import('Model', 'Schedule'); 
-		//debug($this,true);
-		
-	    	    
-	    /*$userprops = $this->Auth->user();
+		//App::import('Model', 'Schedule');
 
-		$task_names = $this->Task->find('list', array(   'fields'=> 'name',    'order'=>'Task.name ASC',  'conditions'=> array('1' => '1'),'group'=>'name')); 
-		$this->set('task_names', $task_names); 
-		
+
+
+		/*$userprops = $this->Auth->user();
+
+		$task_names = $this->Task->find('list', array(   'fields'=> 'name',    'order'=>'Task.name ASC',  'conditions'=> array('1' => '1'),'group'=>'name'));
+		$this->set('task_names', $task_names);
+
 		if ($userprops['User']['group_id'] == 1)
 		{
-			//put the group condition here
-			$emails = $this->User->find('list', array(   'fields'=> 'email',    'order'=>'User.email ASC',  'conditions'=> array('NOT' => array('User.email' => '')))); 
-			$this->set('emails', $emails); 
+		//put the group condition here
+		$emails = $this->User->find('list', array(   'fields'=> 'email',    'order'=>'User.email ASC',  'conditions'=> array('NOT' => array('User.email' => ''))));
+		$this->set('emails', $emails);
 		}
-		else 
+		else
 		{
-			$this->set('emails',array('1'=>$userprops['User']['email']));
+		$this->set('emails',array('1'=>$userprops['User']['email']));
 		}
-		
+
 		$frequencies = $this->Alert->find('list', array(   'fields'=> 'frequency',    'group'=>'frequency'));
 		$this->set('frequencies',$frequencies);*/
 
-		//debug($task_names,true);
+
 	}
 
 
 	function index() {
 		$this->Alert->recursive = 2;
+		$userprops = $this->Auth->user();
+		$conditions = null;
+	
+		
+		
+		
+		/*
+		 * 2 issues:
+		 * 
+		 * 1) If I move the session property login in here, I need a way to conclusively determine if the user
+		 * clicked on the fitler button vs arriving at this page through another mechanism.
+		 * 
+		 * 2) If I keep the logic in app_controller.php, then I need to pass the form data with the redirect.
+		 * 
+		 */
+		
+		
+			
+		/*
+		 * If not in the administrators group, filter the data based off of user.
+		 */
+		
+		if($userprops['User']['group_id'] != 1)
+		{
+			$tmpuserid = $this->User->find('first', array('fields'=> 'id','conditions'=>array('User.username'=>$userprops['User']['username'])));
+			$conditions = array('Alert.user_id'  => $tmpuserid['User']['id']);
+		}
+		
+		
+		if ($this->Session->read('FilterValues') != null)
+		{
+				$filtervalues = $this->Session->read('FilterValues');
+				//debug($filtervalues,true);
+				if (!empty($filtervalues))
+				{
+					if ($filtervalues['Alert']['filtersenabled']=='1')
+					{
+						if ($filtervalues['Alert']['schedule_id']!="All")
+							$conditions['Schedule.id'] = $filtervalues['Alert']['schedule_id'];
+			
+						if (!empty($filtervalues['Entity']['ticker']))
+							$conditions['Entity.ticker LIKE'] = $filtervalues['Entity']['ticker'];
+			
+					}
+				}
+			/*if ($th['Alert']['schedule_id']!="All")
+			{
+				debug('Filter schedules',true);
+				$conditions['Schedule.id'] = $this->data['Alert']['schedule_id'];
+			}
+			
+			if (!empty($filtervalues['Entity']['ticker']))
+				$conditions['Entity.ticker LIKE'] = $filtervalues['Entity']['ticker'];*/
+		}
+		
+
+		$this->paginate = array('conditions' => $conditions);
+
+		$task_names2 = $this->Task->find('list', array('fields'=> array('SchedulesAlias.id','description'),'joins'=>array(
+		array('table'=>'schedules','alias'=>'SchedulesAlias','type'=>'inner','foreignKey'=>false,'conditions'=>array('SchedulesAlias.task_id = Task.id'))
+		)));
+
+		//Add an all option to the front
+		$tmp = array("All"=>"All");
+		foreach(array_keys($task_names2) as $key)
+		{
+			$tmp[$key] = $task_names2[$key];
+		}
+		$task_names2 = $tmp;
+
+		$this->set('task_names2', $task_names2);
+
 		$this->set('alerts', $this->paginate());
-		
-		
+
+
 	}
 
 	function view($id = null) {
@@ -52,154 +136,142 @@ class AlertsController extends AppController {
 			$this->Session->setFlash(__('Invalid alert', true));
 			$this->redirect(array('action' => 'index'));
 		}
+		$this->Alert->recursive = 2;
 		$this->set('alert', $this->Alert->read(null, $id));
 	}
-	
-	function multiAdd() {
-		debug("begin multiAdd",true);
+
+
+
+	function add() {
 		if (!empty($this->data)) {
+			
+			//restructure the data
+			$multiselect = $this->data['Alert']['entity_id'];
+			//$i=0;
+			$tmp2 = array();
+				
+			foreach ($multiselect as $item){
+				$tmp = $this->data['Alert'];
+				$tmp['entity_id'] = $item;
+				array_push($tmp2,$tmp);
+				//$i++;
+			}
+			$this->data['Alert'] = $tmp2;
 			$this->Alert->create();
-			if ($this->Alert->save($this->data)) {
+				
+			
+
+			if ($this->Alert->saveAll($this->data['Alert'])) {
 				$this->Session->setFlash(__('The alert has been saved', true));
 				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The alert could not be saved. Please, try again.', true));
 			}
+				
 		}
 		
-		
-		//debug($userprops,true);
-		
-		$markers = $this->Marker->find('all', array('joins' => array(
-    array(
-        'table' => 'markers_tags',
-        'alias' => 'MarkersTag',
-        'type' => 'inner',
-        'foreignKey' => false,
-        'conditions'=> array('MarkersTag.marker_id = Marker.id')
-    ),
-    array(
-        'table' => 'tags',
-        'alias' => 'Tag',
-        'type' => 'inner',
-        'foreignKey' => false,
-        'conditions'=> array(
-            'Tag.id = MarkersTag.tag_id',
-            'Tag.tag' => explode(' ', $this->params['url']['q'])
-        )
-    )
-)));
-		
+		$task_names2 = $this->Task->find('list', array('fields'=> array('SchedulesAlias.id','description'),'joins'=>array(
+		array('table'=>'schedules','alias'=>'SchedulesAlias','type'=>'inner','foreignKey'=>false,'conditions'=>array('SchedulesAlias.task_id = Task.id'))
+		)));
+		$this->set('task_names2', $task_names2);
+	
 		$task_names = $this->Task->find('list', array('fields'=> 'description',    'order'=>'Task.description ASC',  'conditions'=> array('1' => '1'),'group'=>'description'));
-		$this->set('task_names', $task_names); 
-		
+		$this->set('task_names', $task_names);
+
 		$entity_descs = $this->Entity->find('list', array('fields'=> 'full_name',    'order'=>'Entity.full_name ASC'));
-		$this->set('entity_descs', $entity_descs); 
-		
+		$this->set('entity_descs', $entity_descs);
+
 		//$entities = $this->Entity->find('list', array(''))
 		$userprops = $this->Auth->user();
-		if ($userprops['User']['group_id'] == 1)
-		{
+		if ($userprops['User']['group_id'] == 1){
 			//put the group condition here
-			$names = $this->User->find('list', array(   'fields'=> 'username',    'order'=>'User.username ASC',  'conditions'=> array('NOT' => array('User.username' => '')))); 
-			$this->set('usernames', $names); 
-		}
-		else 
-		{
+			$names = $this->User->find('list', array(   'fields'=> 'username',    'order'=>'User.username ASC',  'conditions'=> array('NOT' => array('User.username' => ''))));
+			$this->set('usernames', $names);
+		}else{
 			$this->set('usernames',array('1'=>$userprops['User']['username']));
 		}
-		
+
 		$frequencies = $this->Alert->find('list', array(   'fields'=> 'frequency',    'group'=>'frequency'));
 		$this->set('frequencies',array('HOURLY'=>'HOURLY','DAILY'=>'DAILY','WEEKLY'=>'WEEKLY','MONTHLY'=>'MONTHLY','YEARLY'=>'YEARLY','ALLTIME'=>'ALLTIME'));
 		//$this->set('limit_adjustment',.2);
 		$this->set('fact_data_key',0);
 		$this->set('alert_count',0);
 		$this->set('type','LIMIT');
-		debug("end multiAdd",true);
-		//$this->set('user',$userprops['User']['id']);
 		
+		//$this->set('user',$userprops['User']['id']);
 	}
 
-	function add() {
-		//debug("begin add",true);
-		debug($this->data, true);
+	function edit($id = null) {
+		$record = $this->Session->read('Record');
+		
+		/*
+		 * Get parameter overrides Post parameter
+		 */
+		if (array_key_exists('alert',$this->params['url']) && !empty($this->params['url']['alert']))
+		{
+			$record = "";
+			$this->Session->delete('Record');
+			$record= array(0 => $this->params['url']['alert']);
+		}
+	
+		
+		
+		
 		if (!empty($this->data)) {
-			$this->Alert->create();
-			if ($this->Alert->save($this->data)) {
+			
+			/*
+			 * Here we need to reformat the data and covert the string ticker to an entity_id.
+			 */
+		
+			for($x=0;$x<sizeof($this->data['Alert']);$x++)
+			{
+				$tmpticker = $this->data['Entity'][$x]['ticker'];
+				$tmpentityid = $this->Entity->find('first', array('fields'=> 'id','conditions'=>array('Entity.ticker'=>$tmpticker)));
+				$this->data['Alert'][$x]['entity_id'] = $tmpentityid['Entity']['id'];
+			}
+			
+			if ($this->Alert->saveAll($this->data['Alert'])) {
 				$this->Session->setFlash(__('The alert has been saved', true));
+				$this->Session->delete('Record');
 				//$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The alert could not be saved. Please, try again.', true));
 			}
 		}
-		
-		
-		//debug($userprops,true);
-		$task_names2 = $this->Task->find('list', array('fields'=> array('SchedulesAlias.id','description'),'joins'=>array(
-		array('table'=>'schedules','alias'=>'SchedulesAlias','type'=>'inner','foreignKey'=>false,'conditions'=>array('SchedulesAlias.task_id = Task.id'))
-		)));
-		$this->set('task_names2', $task_names2); 
-		//debug($task_names2,true);
-		$task_names = $this->Task->find('list', array('fields'=> 'description',    'order'=>'Task.description ASC',  'conditions'=> array('1' => '1'),'group'=>'description'));
-		$this->set('task_names', $task_names); 
-		
-		$entity_descs = $this->Entity->find('list', array('fields'=> 'full_name',    'order'=>'Entity.full_name ASC'));
-		$this->set('entity_descs', $entity_descs); 
-		
-		//$entities = $this->Entity->find('list', array(''))
-		$userprops = $this->Auth->user();
-		if ($userprops['User']['group_id'] == 1)
-		{
-			//put the group condition here
-			$names = $this->User->find('list', array(   'fields'=> 'username',    'order'=>'User.username ASC',  'conditions'=> array('NOT' => array('User.username' => '')))); 
-			$this->set('usernames', $names); 
-		}
-		else 
-		{
-			$this->set('usernames',array('1'=>$userprops['User']['username']));
+		if (!empty($record)) {
+			/*
+			 * This find() call gathers the alert data that is passed to the edit.ctp view.
+			 */
+			
+			$this->data = $this->toMulti($this->Alert->find('all',array('conditions'=>array('Alert.id'=>$record),'recursive'=>2)));
 		}
 		
-		$frequencies = $this->Alert->find('list', array(   'fields'=> 'frequency',    'group'=>'frequency'));
-		$this->set('frequencies',array('HOURLY'=>'HOURLY','DAILY'=>'DAILY','WEEKLY'=>'WEEKLY','MONTHLY'=>'MONTHLY','YEARLY'=>'YEARLY','ALLTIME'=>'ALLTIME'));
-		//$this->set('limit_adjustment',.2);
-		$this->set('fact_data_key',0);
-		$this->set('alert_count',0);
-		$this->set('type','LIMIT');
-		//debug("end add",true);
-		//$this->set('user',$userprops['User']['id']);
-		
-	}
-
-	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid alert', true));
+		if (empty($this->data)) {
+			$this->log('in edit @ invalid user',LOG_DEBUG);
+			$this->Session->setFlash(__('Invalid user', true));
 			$this->redirect(array('action' => 'index'));
 		}
-		if (!empty($this->data)) {
-			if ($this->Alert->save($this->data)) {
-				$this->Session->setFlash(__('The alert has been saved', true));
-				$this->redirect(array('action' => 'index'));
-			} else {
-				$this->Session->setFlash(__('The alert could not be saved. Please, try again.', true));
-			}
-		}
-		if (empty($this->data)) {
-			$this->data = $this->Alert->read(null, $id);
-		}
+		//$task_names = $this->Task->find('list', array('fields'=> 'description',    'order'=>'Task.description ASC',  'conditions'=> array('1' => '1'),'group'=>'description'));
+		$task_names = $this->Task->find('list', array('fields'=> array('SchedulesAlias.id','description'),'joins'=>array(
+		array('table'=>'schedules','alias'=>'SchedulesAlias','type'=>'inner','foreignKey'=>false,'conditions'=>array('SchedulesAlias.task_id = Task.id'))
+		)));
+		$this->set('task_names', $task_names);
 		
-		$task_names = $this->Task->find('list', array('fields'=> 'description',    'order'=>'Task.description ASC',  'conditions'=> array('1' => '1'),'group'=>'description'));
-		$this->set('task_names', $task_names); 
+		$tickers = $this->Entity->find('list', array('fields'=> array('id','ticker')));
+		$this->set('tickers1', $tickers);
 		
+	
+
 		$this->set('frequencies',array('HOURLY'=>'HOURLY','DAILY'=>'DAILY','WEEKLY'=>'WEEKLY','MONTHLY'=>'MONTHLY','YEARLY'=>'YEARLY','ALLTIME'=>'ALLTIME'));
-		
+
 		$userprops = $this->Auth->user();
 		if ($userprops['User']['group_id'] == 1)
 		{
 			//put the group condition here
-			$names = $this->User->find('list', array(   'fields'=> 'username',    'order'=>'User.username ASC',  'conditions'=> array('NOT' => array('User.username' => '')))); 
-			$this->set('usernames', $names); 
+			$names = $this->User->find('list', array(   'fields'=> 'username',    'order'=>'User.username ASC',  'conditions'=> array('NOT' => array('User.username' => ''))));
+			$this->set('usernames', $names);
 		}
-		else 
+		else
 		{
 			$this->set('usernames',array('1'=>$userprops['User']['username']));
 		}
