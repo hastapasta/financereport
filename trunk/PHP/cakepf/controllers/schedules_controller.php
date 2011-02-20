ce745551f99f41f16ddd89b58c28ae8220319a53
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-02-20 21:41:30 +0000

    More development work is needed for this one. In this commit, the add() and edit() actions were updated and the $uses (model linking) was also expanded.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@338 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/cakepf/controllers/schedules_controller.php
+++ trunk/PHP/cakepf/controllers/schedules_controller.php
@@ -2,16 +2,16 @@
 class SchedulesController extends AppController {
 
 	var $name = 'Schedules';
-	var $uses = array('Schedule','Task');
+	var $uses = array('Schedule','Task','RepeatType','EntityGroup');
 
 	function beforeFilter() {
-	    parent::beforeFilter(); 
-	    $this->Auth->allowedActions = array('index', 'view');
+		parent::beforeFilter();
+		$this->Auth->allowedActions = array('index', 'view');
 	}
 
 
 	function index() {
-		$this->Schedule->recursive = 0;
+		$this->Schedule->recursive = 2;
 		$this->set('schedules', $this->paginate());
 	}
 
@@ -25,6 +25,7 @@ class SchedulesController extends AppController {
 
 	function add() {
 		if (!empty($this->data)) {
+			//debug($this->data); exit();
 			$this->Schedule->create();
 			if ($this->Schedule->save($this->data)) {
 				$this->Session->setFlash(__('The schedule has been saved', true));
@@ -33,24 +34,36 @@ class SchedulesController extends AppController {
 				$this->Session->setFlash(__('The schedule could not be saved. Please, try again.', true));
 			}
 		}
+		$tasks = $this->Schedule->Task->find('list');
+		$repeatTypes = $this->Schedule->RepeatType->find('list',array('fields'=>array('type')));
+		$this->set(compact('tasks','repeatTypes'));
 	}
 
 	function edit($id = null) {
-		if (!$id && empty($this->data)) {
-			$this->Session->setFlash(__('Invalid schedule', true));
-			$this->redirect(array('action' => 'index'));
-		}
+		$this->Schedule->recursive = 0;
+		$record = $this->Session->read('Record');
 		if (!empty($this->data)) {
-			if ($this->Schedule->save($this->data)) {
+			if ($this->Schedule->saveAll($this->data['Schedule'])) {
 				$this->Session->setFlash(__('The schedule has been saved', true));
+				$this->Session->delete('Record');
 				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The schedule could not be saved. Please, try again.', true));
 			}
 		}
-		if (empty($this->data)) {
-			$this->data = $this->Schedule->read(null, $id);
+		if (!empty($record)) {
+			$this->data = $this->toMulti($this->Schedule->find('all',array('conditions'=>array('Schedule.id'=>$record))));
 		}
+		if (empty($this->data)) {
+			$this->Session->setFlash(__('Invalid user', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$repeat_types = $this->RepeatType->find('list', array('fields'=> array('RepeatType.description')
+		));
+		$task_names = $this->Task->find('list', array('fields'=> array('Task.name')
+		));
+		$this->set('task_names',$task_names);
+		$this->set('repeat_types',$repeat_types);
 	}
 
 	function delete($id = null) {
