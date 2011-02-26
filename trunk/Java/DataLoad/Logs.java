debc611dd72809cf62077d9d4f1fe6e50a1aa43f
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-02-26 20:04:12 +0000

    -added a WARN message type
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@359 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/DataLoad/Logs.java
+++ trunk/Java/DataLoad/Logs.java
@@ -4,6 +4,6 @@
  * STATUS1 messages are high level and also get written to System.out.println
  * STATUS2 are lower level, more detailed messages and are only written to the log file.
  */
-public enum Logs {FULL,ERROR,SQL,THREAD,STATUS1,STATUS2,NONE} 
+public enum Logs {FULL,ERROR,WARN,SQL,THREAD,STATUS1,STATUS2,NONE} 
 
 
