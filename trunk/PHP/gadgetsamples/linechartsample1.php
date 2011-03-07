a3fe4163bd09d49c0b2514a572b4f4a1d9c4df19
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-07 20:27:15 +0000

    -fixed some errors and created a data set that more sense and had 2 lines.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@408 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/gadgetsamples/linechartsample1.php
+++ trunk/PHP/gadgetsamples/linechartsample1.php
@@ -18,28 +18,36 @@
       google.load('visualization', '1', {packages:['linechart']});
       google.setOnLoadCallback(drawTable);
       function drawTable() {
-        var data = new google.visualization.LineChart();
-        data.addColumn('string', 'Name');
-        data.addColumn('number', 'Salary');
-        data.addColumn('boolean', 'Full Time Employee');
+        var data = new google.visualization.DataTable();
+        alert('here');
+        data.addColumn('string', 'year');
+        data.addColumn('number', 'Mike');
+        data.addColumn('number', 'Susan');
         data.addRows(4);
-        data.setCell(0, 0, 'Mike');
+        data.setCell(0, 0, '2008');
         data.setCell(0, 1, 10000, '$10,000');
-        data.setCell(0, 2, true);
-        data.setCell(1, 0, 'Jim');
+        data.setCell(0, 2, 6000,'$6,000');
+        data.setCell(1, 0, '2009');
         data.setCell(1, 1, 8000, '$8,000');
-        data.setCell(1, 2, false);
-        data.setCell(2, 0, 'Alice');
+        data.setCell(1, 2, 6500,'$6,500');
+        data.setCell(2, 0, '2010');
         data.setCell(2, 1, 12500, '$12,500');
-        data.setCell(2, 2, true);
-        data.setCell(3, 0, 'Bob');
+        data.setCell(2, 2, 9000,'$9,500');
+        data.setCell(3, 0, '2011');
         data.setCell(3, 1, 7000, '$7,000');
-        data.setCell(3, 2, true);
+        data.setCell(3, 2, 10000,'$10,000');
+      
+        var options = {};
+        options['title'] = 'Test Chart Title';
+     	options['height'] = 600;
+     	options['width'] = 800;
+     	
 
         var jsondiv = document.getElementById('jsondata');
     	jsondiv.innerHTML =  data.toJSON();
-        var linechart = new google.visualization.Table(document.getElementById('table_div'));
-        linechart.draw(data, {showRowNumber: true});
+        var linechart = new google.visualization.LineChart(document.getElementById('table_div'));
+        //linechart.draw(data, {showRowNumber: true});
+        linechart.draw(data,options);
       }
     </script>
   </head>
