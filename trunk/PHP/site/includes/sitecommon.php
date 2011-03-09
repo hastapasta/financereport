0f8a08ce715c9271776832ef458c78c1703750c2
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-09 19:05:26 +0000

    -testing deleting a file and then committing -also updated how the primary nav urls are built
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@419 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/site/includes/sitecommon.php
+++ trunk/PHP/site/includes/sitecommon.php
@@ -13,14 +13,14 @@ function primaryNav($Page)
 		
 	
 	
-	
+	$ROOT_PATH="/phptest";
 	
 	
 	echo "<div id=\"jq-primaryNavigation\">\n";
 	echo "<ul>\n";
-	echo "<li class=\"jq-home ".($Page=="home"?"jq-current":"")."\"><a href=\"/PHP/site/main/index.php\" title=\"Pikefin Home\">Home</a></li>\n";
-	echo "<li class=\"jq-alerts ".($Page=="alerts"?"jq-current":"")."\"><a href=\"/PHP/site/main/alerts.php\" title=\"Pikefin Alert Manager\">Alert Manager</a></li>\n";
-	echo "<li class=\"jq-charts ".($Page=="charts"?"jq-current":"")."\"><a href=\"/PHP/charts/fed/balsheet.php\" title=\"Pikefin Charts\">Charts</a></li>";
+	echo "<li class=\"jq-home ".($Page=="home"?"jq-current":"")."\"><a href=\"".$ROOT_PATH."/site/main/index.php\" title=\"Pikefin Home\">Home</a></li>\n";
+	echo "<li class=\"jq-alerts ".($Page=="alerts"?"jq-current":"")."\"><a href=\"".$ROOT_PATH."/site/main/alerts.php\" title=\"Pikefin Alert Manager\">Alert Manager</a></li>\n";
+	echo "<li class=\"jq-charts ".($Page=="charts"?"jq-current":"")."\"><a href=\"".$ROOT_PATH."/charts/fed/balsheet.php\" title=\"Pikefin Charts\">Charts</a></li>";
 	//echo "<li class=\"jq-blog ".($Page=="blog"?"jq-current":"")."\"><a href=\"/PHP/site/main/blog.php\" title=\"Pikefin Blog\">Blog</a></li>";
 	//echo "<li class=\"jq-about ".($Page=="about"?"jq-current":"")."\"><a href=\"/PHP/site/main/about.php\" title=\"About Pikefin\">About</a></li>";
 	echo "</ul>\n";
@@ -36,11 +36,11 @@ function chartSecondaryNav()
 	
 	echo "<div id=\"jq-secondaryNavigation\">\n";
 	echo "<ul>\n";
-	echo "<li class=\"jq-fedreserve jq-first\"><a href=\"../../charts/fed/balsheet.php\">Federal Reserve Main</a></li>\n";
-	echo "<li class=\"jq-allassets\"><a href=\"../../charts/allassets/tablesample3A.php\">All Assets</a></li>\n";
-	echo "<li class=\"jq-forex\"><a href=\"../../charts/forex/forex.php\">Foreign Exchange</a></li>\n";
-	echo "<li class=\"jq-commodities\"><a href=\"../../charts/commods/commodities.php\">Commodities</a></li>\n";
-	echo "<li class=\"jq-equities jq-last\"><a href=\"../../charts/usequities/usequities.php\">U.S Equities</a></li>\n";
+	echo "<li class=\"jq-fedreserve jq-first\"><a href=\"".$ROOT_PATH."/charts/fed/balsheet.php\">Federal Reserve Main</a></li>\n";
+	echo "<li class=\"jq-allassets\"><a href=\"".$ROOT_PATH."/charts/allassets/tablesample3A.php\">All Assets</a></li>\n";
+	echo "<li class=\"jq-forex\"><a href=\"".$ROOT_PATH."/charts/forex/forex.php\">Foreign Exchange</a></li>\n";
+	echo "<li class=\"jq-commodities\"><a href=\"".$ROOT_PATH."/charts/commods/commodities.php\">Commodities</a></li>\n";
+	echo "<li class=\"jq-equities jq-last\"><a href=\"".$ROOT_PATH."/charts/usequities/usequities.php\">U.S Equities</a></li>\n";
 	echo "</ul>\n";
 	echo "</div><!-- /#secondaryNavigation -->\n";
 	
