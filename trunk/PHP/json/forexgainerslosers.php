87efc6eb02d832a09aa872b0187c961528f1f757
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-14 23:59:54 +0000

    -added some http request parameter verification logic and updated some database references
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@450 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/json/forexgainerslosers.php
+++ trunk/PHP/json/forexgainerslosers.php
@@ -1,16 +1,42 @@
 <?php
 
-	mysql_connect("testdataload","root","madmax1.") or die(mysql_error());
+	mysql_connect("localhost","root","madmax1.") or die(mysql_error());
 	mysql_select_db("findata") or die(mysql_error());
 	//$query="SELECT * FROM company WHERE ticker='".$_REQUEST['ticker']."'";
 	
-	$query="SELECT AL3.value2, AL3.date_collected2, AL1.value, AL1.date_collected, AL2.ticker, AL2.frequency, ".
-	"(AL3.value2 - AL1.value)/AL3.value2 AS changeval FROM fact_data AL1, notify AL2, ".
+	$rows=10;
+	if (isset($_REQUEST['rows']))
+	{
+	
+		//$query=$queryweaker;
+		$rows=$_REQUEST['rows'];
+		
+	}
+	
+	$frequency="YEARLY";
+	if (isset($_REQUEST['freq']))
+	{
+		$frequency=$_REQUEST['freq'];
+	}
+	
+	
+		
+	/*$query = "SELECT AL3.value2, AL3.date_collected2, AL1.value, AL1.date_collected, AL2.ticker, AL2.frequency, ".
+	"(AL3.value2 - AL1.value)/AL1.value2 AS changeval FROM fact_data AL1, notify AL2, ".
 	"(SELECT D2AL2.value AS value2, D2AL2.date_collected AS date_collected2, D2AL1.ticker AS ticker from fact_data D2AL2, ". 
 	"(select max(batch) mx, ticker from fact_data where data_set like '%xrateorg%' group by ticker) as D2AL1  ".
 	"where data_set like '%xrateorg%' and D2AL2.batch=D2AL1.mx and D2AL1.ticker=D2AL2.ticker) AL3 ".
 	"WHERE (AL1.primary_key=AL2.fact_data_key AND AL3.ticker=AL1.ticker)  AND ".
-	"(AL1.data_set LIKE '%xrateorg%' AND AL1.ticker LIKE 'USD%' AND AL2.frequency='YEARLY' AND AL2.email='opike@yahoo.com') ORDER BY  7 {order} limit 10";
+	"(AL1.data_set LIKE '%xrateorg%' AND AL1.ticker LIKE 'USD%' AND AL2.frequency='".$frequency."' AND AL2.email='opike@yahoo.com') ORDER BY  7 {order} limit ".$rows;*/
+	
+	$query = "SELECT AL3.value2, AL3.date_collected2, AL1.value, AL1.date_collected, AL4.ticker, AL2.frequency, ".
+	"(AL3.value2 - AL1.value)/AL1.value AS changeval FROM fact_data AL1, alerts AL2,entities AL4, ".
+	"(SELECT D2AL2.value AS value2, D2AL2.date_collected AS date_collected2, D2AL2.entity_id from fact_data D2AL2, ". 
+	"(select max(batch) mx from fact_data where fact_data.task_id=1) as D2AL1  ".
+	"where task_id=1 and D2AL2.batch=D2AL1.mx) AL3 ".
+	"WHERE (AL1.id=AL2.fact_data_key AND AL3.entity_id=AL1.entity_id AND AL1.entity_id=AL4.id  AND ".
+	"AL1.task_id=1 AND AL4.ticker LIKE 'USD%' AND AL2.frequency='".$frequency."' AND AL2.user_id=4) ORDER BY  7 {order} limit ".$rows;
+	
 
 	
 	
