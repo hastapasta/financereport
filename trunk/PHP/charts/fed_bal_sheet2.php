aa5b6bbf5ba1c6e0668f7ee0275b88e16fc63f7a
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-19 00:01:06 +0000

    -deleted
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@493 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/charts/fed_bal_sheet2.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-<table>
-<tr><td>
-<script src="https://spreadsheets.google.com/gpub?url=http%3A%2F%2Ff62ed6fg3rppptjtj5ia5idjichr25gm-ss-opensocial.googleusercontent.com%2Fgadgets%2Fifr%3Fup_title%26up_chartTitle%3DFederal%2520Reserve%2520Balance%2520Sheet%2520-%2520Assets%26up_labelx%26up_labely%3D(%2524%2520millions)%26up_legend%3D0%26up_stacked%3D1%26up_showpoints%3D1%26up_min%26up_max%26up__table_query_url%3Dhttps%253A%252F%252Fspreadsheets.google.com%252Ftq%253Fgid%253D1%2526range%253DA1%25253AV9%2526key%253D0AvI-SyuXTqm8dDc3OUU5ZzBRSVVhR2VNRkxJTmhlM3c%2526pub%253D1%26url%3Dhttp%253A%252F%252Fwww.google.com%252Fig%252Fmodules%252Farea-chart.xml%26spreadsheets%3Dspreadsheets&height=500&width=1200"></script>
-</td></tr>
-</table>
-</body>
-</html>
