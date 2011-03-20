d5f4b01a1c0db0291b3728c2c9e4867bddaf14d9
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-20 01:04:58 +0000

    -added function to combine task_id=1 batches.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@497 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/ScrubDatabase/pikefin/ScrubDB.java
+++ trunk/Java/ScrubDatabase/pikefin/ScrubDB.java
@@ -1,8 +1,12 @@
 package pikefin;
 
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 
 public class ScrubDB {
 
@@ -10,7 +14,7 @@ public class ScrubDB {
 	 * @param args
 	 */
 	
-	public static void main(String[] args) {
+	public static void main2(String[] args) {
 		
 		try
 		{
@@ -73,6 +77,158 @@ public class ScrubDB {
 		
 		
 	
+	}
+	
+	public static void main(String[] args) {
+		
+		try
+		{
+			DBFunctions dbf = new DBFunctions("localhost","3306","findata","root","madmax1.");
+			
+			UtilityFunctions uf = new UtilityFunctions();
+			
+			
+		    //int nStartBatchNumber = 1529;
+			int nStartBatchNumber = 9410;
+		    
+		    String query = "select distinct batch from fact_data where task_id=1 and batch>" + nStartBatchNumber + " and batch<25662 order by batch ASC";
+		    
+		    ResultSet rs = dbf.db_run_query(query);
+		    
+		    String[] batch = new String[5];
+		    
+		    while (rs.next())
+		    {
+		    	
+		    	
+		    	batch[0] = rs.getInt("batch") + "";
+		    	rs.next();
+		    	batch[1] = rs.getInt("batch") + "";
+		    	rs.next();
+		    	batch[2] = rs.getInt("batch")  + "";
+		    	rs.next();
+		    	batch[3] = rs.getInt("batch")  + "";
+		    	rs.next();
+		    	batch[4] = rs.getInt("batch") + "";
+		    	
+		    	String query2 = "select count(id) as cnt from fact_data where batch in (" + batch[0] + "," + batch[1] + "," + batch[2] + "," + batch[3] + "," + batch[4] + ")";
+		    	
+		    	ResultSet rs2 = dbf.db_run_query(query2);
+		    	
+		    	rs2.next();
+		    	
+		    	if (rs2.getInt("cnt") != 120)
+		    	{
+		    		System.out.println("Group of batches doesn't add up to 120. Terminating.");
+		    		//break;
+		    		continue;
+		    	}
+		    	
+		    	/*
+		    	 * 
+		    	 * Checking that batch data_collected value is within a certain range.
+		    	 */
+		    	Calendar[] calDates = new Calendar[5];
+		    	SimpleDateFormat inputFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		    	
+		    	for (int i=0;i<5;i++)
+		    	{
+		    	
+			    	String query4 = "select max(date_collected) as dc from fact_data where batch=" + batch[i];
+			    	
+			    	ResultSet rs4 = dbf.db_run_query(query4);
+			    	
+			    	rs4.next();
+			    				    	
+			    	calDates[i] = Calendar.getInstance();
+			    	
+			    	
+			    	try
+			    	{
+			    		calDates[i].setTime(inputFormat.parse(rs4.getString("dc")));		    		
+			    				    		
+			    		
+			    	}
+			    	catch (ParseException pe)
+			    	{
+			    		System.out.println(pe.getMessage());
+					  
+			    	}
+			    	
+		    	
+		    	
+		    	}
+		    	
+		    	calDates[0].add(Calendar.MINUTE,12);
+		    	
+		    	for (int j=1;j<5;j++)
+		    	{
+		    		if (calDates[j].after(calDates[0]))
+		    		{
+		    			System.out.println("Batch out of time range.");
+		    			
+		    			//return;
+		    		}
+		    		
+		    		
+		    	}
+		    	
+		    	
+		    	/*
+		    	 * 
+		    	 * Checking that the ticker groups are all distinct.
+		    	 * 
+		    	 */
+		    	
+		    	
+		    	String query6 = "select count(fact_data.id) as cnt from fact_data,entities where batch in (" + batch[0] + "," + batch[1] + "," + batch[2] + "," + batch[3] + "," + batch[4] + ") ";
+		    	query6 += " AND fact_data.entity_id=entities.id ";
+		    	query6 += " AND entities.ticker in ('USDANG','USDAUD','EURGBP','USDAED','USDBIF')";
+		    	
+		    	ResultSet rs6 = dbf.db_run_query(query6);
+		    	
+		    	rs6.next();
+		    	
+		    	if (rs6.getInt("cnt") != 5)
+		    	{
+		    		System.out.println("Groups aren't distinct.Skipping.");
+		    		continue;
+		    	}
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	
+		    	for (int i=1; i<5;i++)
+		    	{
+		    		
+		    		System.out.println("Updating batch " + batch[i] + " to batch " + batch[0]);
+		    		String query3 = "update fact_data set batch=" + batch[0] + " where batch=" + batch[i];
+		    		
+		    		//dbf.db_update_query(query3);
+		    				    		    		
+		    		
+		    	}
+		    	
+		    	
+		    	
+		    	
+		    	
+		    }
+		    
+		}
+		catch (SQLException sqle)
+		{
+			System.out.println(sqle.getMessage());
+		}
+			
+		
+		
 	}
 	public static void main1(String[] args) {
 		// TODO Auto-generated method stub
