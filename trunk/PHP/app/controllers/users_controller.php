709c5847e8bfc334ca5d044a9685b546ccf2a35f
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-21 18:46:15 +0000

    -added color property to setFlash() calls (logiciel) -added login property to this line: $this->Auth->allow(array('logout','login')); (logiciel) -added change_password() function (logiciel)
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@505 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/app/controllers/users_controller.php
+++ trunk/PHP/app/controllers/users_controller.php
@@ -9,13 +9,13 @@ class UsersController extends AppController {
 		 */
 		parent::beforeFilter();
 
-		$this->Auth->allow('logout');
+		$this->Auth->allow(array('logout','login'));
 
 		/*
 		 * End Production setup.
 		 */
 
-		//$this->Auth->allow(array('*'));
+		//$this->Auth->allow('*');
 	}
 
 
@@ -36,7 +36,7 @@ class UsersController extends AppController {
 		if (!empty($this->data)) {
 			$this->User->create();
 			if ($this->User->save($this->data)) {
-				$this->Session->setFlash(__('The user has been saved', true));
+				$this->Session->setFlash(__('The user has been saved', true),'default',array('class'=>'green_message'));
 				$this->redirect(array('action' => 'index'));
 			} else {
 				$this->Session->setFlash(__('The user could not be saved. Please, try again.', true));
@@ -58,7 +58,7 @@ class UsersController extends AppController {
 				$this->data['User'][$i]['password'] = $this->Auth->password($this->data['User'][$i]['password']);
 			}
 			if ($this->User->saveAll($this->data['User'])) {
-				$this->Session->setFlash(__('The user has been saved', true));
+				$this->Session->setFlash(__('The user has been saved', true),'default',array('class'=>'green_message'));
 				$this->Session->delete('Record');
 				//$this->redirect(array('action' => 'index'));
 			} else {
@@ -66,6 +66,7 @@ class UsersController extends AppController {
 			}
 		}
 		if (!empty($record)) {
+			//$this->data = $this->toMulti($this->User->find('all',array('conditions'=>array('User.id'=>$record))));
 			//$this->data = $this->User->read(null, $id);
 			if($userprops['User']['group_id'] == 1)
 			{
@@ -78,8 +79,7 @@ class UsersController extends AppController {
 			{
 				$this->data = $this->toMulti($this->User->find('all',array('conditions'=>array('User.id'=>$userprops['User']['id']))));
 				$this->set('administrator', false);
-			}
-				
+			}	
 				
 		}
 		if (empty($this->data)) {
@@ -89,8 +89,6 @@ class UsersController extends AppController {
 		//$groups = $this->User->Group->find('list');
 		//$this->set(compact('groups'));
 		
-		
-		
 	}
 
 	function delete($id = null) {
@@ -99,7 +97,7 @@ class UsersController extends AppController {
 			$this->redirect(array('action'=>'index'));
 		}
 		if ($this->User->delete($id)) {
-			$this->Session->setFlash(__('User deleted', true));
+			$this->Session->setFlash(__('User deleted', true),'default',array('class'=>'green_message'));
 			$this->redirect(array('action'=>'index'));
 		}
 		$this->Session->setFlash(__('User was not deleted', true));
@@ -108,18 +106,50 @@ class UsersController extends AppController {
 
 	function login() {
 		if ($this->Session->read('Auth.User')) {
-			$this->Session->setFlash('You are logged in!');
+			$this->Session->setFlash('You are logged in!','default',array('class'=>'green_message'));
 				
 			//$this->redirect('/alerts', null, false);
 		}
 	}
 
 	function logout() {
-		$this->Session->setFlash('Good-Bye');
+		$this->Session->setFlash('Good-Bye','default',array('class'=>'green_message'));
 		$this->redirect($this->Auth->logout());
 	}
-
-	/*function initDB() {
+	
+	function change_password(){
+		if(!empty($this->data)){
+			$cansave = true;
+			$user = $this->User->findByUsername($this->Auth->user('username'));
+			if(!$user){
+				$this->User->validationErrors['username'] = 'Username not found';
+				$cansave = false;
+			}
+			if($user && $user['User']['password'] != $this->Auth->password($this->data['User']['old_password']) ){
+				$this->User->validationErrors['old_password'] = 'Password is incorrect';
+				$cansave = false;
+			}
+			if($this->data['User']['new_password'] != $this->data['User']['confirm_password']){
+				$this->User->validationErrors['confirm_password'] = "Password didn't match";
+				$cansave = false;
+			}
+			if($cansave){
+				$user['User']['password' ] = $this->Auth->password($this->data['User']['new_password']);
+				if($this->User->save($user)){
+					$this->Session->setFlash(__('Password is changed successfully' , true),'default',array('class'=>'green_message'));					
+				}else{
+					$this->Session->setFlash(__("password could not be changed",true));
+				}
+			}
+			
+		}
+	}
+	
+	function chart(){
+		
+	}
+/*
+	function initDB() {
 		$group =& $this->User->Group;
 		//Allow admins to everything
 		$group->id = 1;
@@ -135,7 +165,7 @@ class UsersController extends AppController {
 		$group->id = 3;
 		$this->Acl->deny($group, 'controllers');
 		$this->Acl->allow($group, 'controllers/Alerts');
-		//$this->Acl->allow($group, 'controllers/Alerts/edit');
+		$this->Acl->allow($group, 'controllers/users/change_password');
 
 		//we add an exit to avoid an ugly "missing views" error message
 		echo "all done";
