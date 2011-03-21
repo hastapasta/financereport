033da8d7b84739b2694f748a08166c1b68aed6f3
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-21 18:56:53 +0000

    -deleted the $validate property (logiciel) -added hasAndBelongsToMany EntityGroup (logiciel)
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@506 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/app/models/entity.php
+++ trunk/PHP/app/models/entity.php
@@ -1,18 +1,7 @@
 <?php
 class Entity extends AppModel {
 	var $name = 'Entity';
-	var $validate = array(
-		'ticker' => array(
-			'notempty' => array(
-				'rule' => array('notempty'),
-	//'message' => 'Your custom message here',
-	//'allowEmpty' => false,
-	//'required' => false,
-	//'last' => false, // Stop validation after this rule
-	//'on' => 'create', // Limit validation to 'create' or 'update' operations
-	),
-	),
-	);
+	//var $displayField = 'ticker';
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
 	var $hasMany = array(
@@ -28,8 +17,79 @@ class Entity extends AppModel {
 			'exclusive' => '',
 			'finderQuery' => '',
 			'counterQuery' => ''
-			)
-			);
+		),
+		/*'FactDatum' => array(
+			'className' => 'FactDatum',
+			'foreignKey' => 'entity_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),*/
+		/*'FactDataStageEst' => array(
+			'className' => 'FactDataStageEst',
+			'foreignKey' => 'entity_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),
+		'FactDataStageEstSave' => array(
+			'className' => 'FactDataStageEstSave',
+			'foreignKey' => 'entity_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		),
+		'LogAlert' => array(
+			'className' => 'LogAlert',
+			'foreignKey' => 'entity_id',
+			'dependent' => false,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)*/
+	);
+
+
+	var $hasAndBelongsToMany = array(
+		'EntityGroup' => array(
+			'className' => 'EntityGroup',
+			'joinTable' => 'entities_entity_groups',
+			'foreignKey' => 'entity_id',
+			'associationForeignKey' => 'entity_group_id',
+			'unique' => true,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'finderQuery' => '',
+			'deleteQuery' => '',
+			'insertQuery' => ''
+		)
+	);
 
 }
 ?>
