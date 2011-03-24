6003fc7a8535370d8c28ee5dfaa8703cbd373cab
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-24 18:40:22 +0000

    Changed directory structure.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@542 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

---
+++ trunk/PHP/charts/other/comingsoon.php
@@ -0,0 +1,33 @@
+<?php
+
+include ("../../site/includes/sitecommon.php");
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<?php IncFunc::icon();?>
+<?php IncFunc::linkStyleCSS(); ?>
+<?php IncFunc::yuiDropDownJavaScript(); ?>
+
+</head>
+
+<body>
+<div id="jq-siteContain">
+<?php  IncFunc::header1("charts"); ?> 
+<?php  IncFunc::yuiDropDownMenu(); ?>
+
+<!-- <div id="jq-intro"> -->
+
+<BR/><BR/><BR/><BR/>
+<div id="tmpsize" style="font-size: medium">
+<h2>Pike Financial Data Services</h2>
+<BR/><BR/>
+
+<p>This chart is in the process of being developed. We hope to have it completed soon...</p>
+</div>
+<!--  </div> --> <!-- intro -->
+</div> <!--  siteContain -->
+</body>
+</HTML>
