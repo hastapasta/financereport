0f76166ecd5a2436d18088ca309885a3faf25b57
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-24 18:46:51 +0000

    -added page counter code -adjusted the path references
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@548 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/site/includes/sitecommon.php
+++ trunk/PHP/site/includes/sitecommon.php
@@ -5,16 +5,23 @@
 class IncFunc
 {
 	
-	static public $ROOT_PATH="/PHP";
-	static public $JSP_ROOT_PATH="http://localhost:8080/JSPDataSource";
+	//static public $PHP_ROOT_PATH="/PHP";
+	static public $CAKE_ROOT_PATH="/cakepftest";
+	//static public $JSP_ROOT_PATH="http://localhost:8080/JSPDataSource";
 	
-	//static public $ROOT_PATH="/phptest";
-	//static public $JSP_ROOT_PATH="http://www.pikefin.com/testjsp/JSPDataSource";
+	static public $PHP_ROOT_PATH="/phptest";
+	static public $JSP_ROOT_PATH="http://www.pikefin.com/testjsp/JSPDataSource";
+	
+	static function icon()
+	{
+		echo "<link href=\"".self::$PHP_ROOT_PATH."/site/images/favicon.ico\" type=\"image/x-icon\" rel=\"icon\" /><link href=\"".self::$PHP_ROOT_PATH."/site/images/favicon.ico\" type=\"image/x-icon\" rel=\"shortcut icon\" />";
+		
+	}
 	
 	static function linkStyleCSS()
 	{
 		
-		echo "<link rel=\"stylesheet\" href=\"".self::$ROOT_PATH."/site/includes/style.css\" type=\"text/css\" />"; 
+		echo "<link rel=\"stylesheet\" href=\"".self::$PHP_ROOT_PATH."/site/includes/style.css\" type=\"text/css\" />"; 
 	}
 
 	static function logo()
@@ -28,9 +35,9 @@ class IncFunc
 
 		echo "<div id=\"jq-primaryNavigation\">\n";
 		echo "<ul>\n";
-		echo "<li class=\"jq-home ".($Page=="home"?"jq-current":"")."\"><a href=\"".self::$ROOT_PATH."/site/main/index.php\" title=\"Pikefin Home\">Home</a></li>\n";
-		echo "<li class=\"jq-alerts ".($Page=="alerts"?"jq-current":"")."\"><a href=\"".self::$ROOT_PATH."/site/main/alerts.php\" title=\"Pikefin Alert Manager\">Alert Manager</a></li>\n";
-		echo "<li class=\"jq-charts ".($Page=="charts"?"jq-current":"")."\"><a href=\"".self::$ROOT_PATH."/charts/fed/balsheet.php\" title=\"Pikefin Charts\">Charts</a></li>";
+		echo "<li class=\"jq-home ".($Page=="home"?"jq-current":"")."\"><a href=\"".self::$PHP_ROOT_PATH."/site/main/index.php\" title=\"Pikefin Home\">Home</a></li>\n";
+		echo "<li class=\"jq-alerts ".($Page=="alerts"?"jq-current":"")."\"><a href=\"".self::$CAKE_ROOT_PATH."/users/chart\" title=\"Pikefin Alert Manager\">Alert Manager</a></li>\n";
+		echo "<li class=\"jq-charts ".($Page=="charts"?"jq-current":"")."\"><a href=\"".self::$PHP_ROOT_PATH."/charts/allassets/topchart.php\" title=\"Pikefin Charts\">Charts</a></li>";
 		//echo "<li class=\"jq-blog ".($Page=="blog"?"jq-current":"")."\"><a href=\"/PHP/site/main/blog.php\" title=\"Pikefin Blog\">Blog</a></li>";
 		//echo "<li class=\"jq-about ".($Page=="about"?"jq-current":"")."\"><a href=\"/PHP/site/main/about.php\" title=\"About Pikefin\">About</a></li>";
 		echo "</ul>\n";
@@ -44,15 +51,51 @@ class IncFunc
 	static function incJquery()
 	{
 		
-		echo "<script src=\"".self::$ROOT_PATH."/site/includes/jquery-1.5.1.js\" type=\"text/javascript\"></script>";
+		echo "<script src=\"".self::$PHP_ROOT_PATH."/site/includes/jquery-1.5.1.js\" type=\"text/javascript\"></script>";
 	}
 	
 	static function header1($context)
 	{
-		echo "<div id=\"jq-header\">";
+		//require_once '../../common/functions.php';
+		echo "<div id=\"jq-header\" >";
 		self::logo();
 		self::primaryNav($context);
+		//self::pageCounter();
 		echo "</div> <!-- header -->";
+	}
+	
+	
+	static function pageCounter()
+	{
+		
+		
+		$uri = $_SERVER['REQUEST_URI'];
+		
+		if (strpos($uri,'?')!=false)
+			$uri = substr($uri,0,strpos($uri,'?'));
+		
+		db_utility::db_connect();
+		
+		$query1 = "select hits from page_counters where uri='".$uri."'";
+		
+		$result1 = mysql_query($query1) or die("Failed Query of " . $query1);
+		
+		$row1 = mysql_fetch_array($result1);
+		
+		if ($row1==null)
+		{
+			$query2 = "insert into page_counters (uri,hits) values ('".$uri."',1)";
+		}
+		else 
+		{
+			$query2 = "update page_counters set hits=".($row1['hits']+1)." where uri='".$uri."'";
+		}
+		
+		mysql_query($query2) or die("Failed Query of " . $query2);
+		
+		
+		
+		
 	}
 	
 	static function chartSecondaryNav()
@@ -60,11 +103,11 @@ class IncFunc
 		
 		echo "<div id=\"jq-secondaryNavigation\">\n";
 		echo "<ul>\n";
-		echo "<li class=\"jq-fedreserve jq-first\"><a href=\"".self::$ROOT_PATH."/charts/fed/balsheet.php\">Federal Reserve Main</a></li>\n";
-		echo "<li class=\"jq-allassets\"><a href=\"".self::$ROOT_PATH."/charts/allassets/tablesample3A.php\">All Assets</a></li>\n";
-		echo "<li class=\"jq-forex\"><a href=\"".self::$ROOT_PATH."/charts/forex/forex.php\">Foreign Exchange</a></li>\n";
-		echo "<li class=\"jq-commodities\"><a href=\"".self::$ROOT_PATH."/charts/commods/commodities.php\">Commodities</a></li>\n";
-		echo "<li class=\"jq-equities jq-last\"><a href=\"".self::$ROOT_PATH."/charts/usequities/usequities.php\">U.S Equities</a></li>\n";
+		echo "<li class=\"jq-fedreserve jq-first\"><a href=\"".self::$PHP_ROOT_PATH."/charts/fed/balsheet.php\">Federal Reserve Main</a></li>\n";
+		echo "<li class=\"jq-allassets\"><a href=\"".self::$PHP_ROOT_PATH."/charts/allassets/tablesample3A.php\">All Assets</a></li>\n";
+		echo "<li class=\"jq-forex\"><a href=\"".self::$PHP_ROOT_PATH."/charts/forex/forex.php\">Foreign Exchange</a></li>\n";
+		echo "<li class=\"jq-commodities\"><a href=\"".self::$PHP_ROOT_PATH."/charts/commods/commodities.php\">Commodities</a></li>\n";
+		echo "<li class=\"jq-equities jq-last\"><a href=\"".self::$PHP_ROOT_PATH."/charts/usequities/usequities.php\">U.S Equities</a></li>\n";
 		echo "</ul>\n";
 		echo "</div><!-- /#secondaryNavigation -->\n";
 		
@@ -74,17 +117,17 @@ class IncFunc
 	{
 		
 		echo "	<!-- Combo-handled YUI CSS files: -->";
-		echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://yui.yahooapis.com/combo?2.8.2r1/build/reset-fonts-grids/reset-fonts-grids.css\">";
+		//echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://yui.yahooapis.com/combo?2.8.2r1/build/reset-fonts-grids/reset-fonts-grids.css\">";
 		echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://yui.yahooapis.com/combo?2.8.2r1/build/menu/assets/skins/sam/menu.css\">";
 		echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://yui.yahooapis.com/combo?2.8.2r1/build/button/assets/skins/sam/button.css\">";
 		echo "<!-- Combo-handled YUI JS files: -->";
 		echo "<script type=\"text/javascript\" src=\"http://yui.yahooapis.com/combo?2.8.2r1/build/yuiloader-dom-event/yuiloader-dom-event.js&2.8.2r1/build/container/container_core-min.js&2.8.2r1/build/menu/menu-min.js&2.8.2r1/build/element/element-min.js&2.8.2r1/build/button/button-min.js\"></script>";
 		
-		echo "<script type=\"text/javascript\" src=\"".self::$ROOT_PATH."/site/includes/yuiDropDown.js\"></script>";
+		echo "<script type=\"text/javascript\" src=\"".self::$PHP_ROOT_PATH."/site/includes/yuiDropDown.js\"></script>";
 		
 		echo "<script type=\"text/javascript\">";
 		
-		echo "var rootpath='".self::$ROOT_PATH."';";
+		echo "var rootpath='".self::$PHP_ROOT_PATH."';";
 		//echo "var rootpath='/PHP';";
 		//echo "var rootpath='/phptest';";
 		
@@ -96,8 +139,8 @@ class IncFunc
 	
 	static function yuiDropDownMenu()
 	{
-		
-		echo "<div class=\"yui-skin-sam\" id=\"yahoo-com\">";
+
+		echo "<div class=\"yui-skin-sam\" id=\"yahoo-com\" style=\"padding: 0 160px;\">";
 		echo "	<div id=\"doc\" class=\"yui-t1\" width=\"30em\">";
 		echo "		<div id=\"productsandservices\" class=\"yuimenubar yuimenubarnav\">";
 		echo "			<div class=\"bd\">";
@@ -119,6 +162,7 @@ class IncFunc
 		echo "		</div>";
 		echo "	</div>";
 		echo "</div>";
+		
 		
 		
 		
