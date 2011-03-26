c3b94d0665a09ee51ba4cdf4eda2c09ea69170fe
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-26 16:04:33 +0000

    -modified the classpath for the DATAGRABALIVE test
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@559 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/MultiTest/run.sh
+++ trunk/Java/MultiTest/run.sh
@@ -1,3 +1,3 @@
 JAVALIB=/home/ollie/dev/java/libraries
-CLASSPATH=.:$JAVALIB/commons-io-1.4/commons-io-1.4.jar:$JAVALIB/httpcomponents-client-4.0.3/lib/httpclient-4.0.3.jar:$JAVALIB/httpcomponents-client-4.0.3/lib/httpmime-4.0.3.zip:$JAVALIB/httpcomponents-core-4.0.1/lib/httpcore-4.0.1.jar:$JAVALIB/commons-logging-1.1.1/commons-logging-1.1.1.jar
+CLASSPATH=.:$JAVALIB/commons-io-1.4/commons-io-1.4.jar:$JAVALIB/httpcomponents-client-4.0.3/lib/httpclient-4.0.3.jar:$JAVALIB/httpcomponents-client-4.0.3/lib/httpmime-4.0.3.zip:$JAVALIB/httpcomponents-core-4.0.1/lib/httpcore-4.0.1.jar:$JAVALIB/commons-logging-1.1.1/commons-logging-1.1.1.jar:$JAVALIB/apache-log4j-1.2.16/log4j-1.2.16.jar:$JAVALIB/mysql-connector-java-5.1.12-bin.jar
 java -classpath $CLASSPATH MultiTest $1 $2  2>error 
