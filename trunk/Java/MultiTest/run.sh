27b0f427de7716d56ac1729936be14631bd556cd
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-03-26 15:27:44 +0000

    Modified this file to do some tests and never changed it back.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@558 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/MultiTest/run.sh
+++ trunk/Java/MultiTest/run.sh
@@ -1,3 +1,3 @@
 JAVALIB=/home/ollie/dev/java/libraries
 CLASSPATH=.:$JAVALIB/commons-io-1.4/commons-io-1.4.jar:$JAVALIB/httpcomponents-client-4.0.3/lib/httpclient-4.0.3.jar:$JAVALIB/httpcomponents-client-4.0.3/lib/httpmime-4.0.3.zip:$JAVALIB/httpcomponents-core-4.0.1/lib/httpcore-4.0.1.jar:$JAVALIB/commons-logging-1.1.1/commons-logging-1.1.1.jar
-java -classpath $CLASSPATH MultiTest LINKTEST 'http://www.pikefin.com/testjsp/JSPDataSource/mysqldatasource7.jsp?taskid=22' $1>out $2>error 
+java -classpath $CLASSPATH MultiTest $1 $2  2>error 
