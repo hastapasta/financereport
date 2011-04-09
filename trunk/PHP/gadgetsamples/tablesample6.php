60e7020c90baab07fc958d6ba899810e6f89c725
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-04-09 00:59:04 +0000

    changed the datasourceUrl to use the IncFunc JSP_ROOT_PATH instead of the db_utility one.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@615 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/gadgetsamples/tablesample6.php
+++ trunk/PHP/gadgetsamples/tablesample6.php
@@ -24,7 +24,7 @@ $taskid = $_GET['taskid'];
     var firstpass = true;
 
     <?php 
-            echo "var dataSourceUrl = '".db_utility::$datasourceurl."mysqldatasource2.jsp';";
+            echo "var dataSourceUrl = '".IncFunc::$JSP_ROOT_PATH."mysqldatasource2.jsp';";
             ?>
         // var dataSourceUrl = 'http://www.pikefin.com/phpdev/gadgetsamples/echodatasource2.php';
            
