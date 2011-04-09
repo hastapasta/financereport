5a81030cd932d71acb68088303488f30c9fdae7e
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-04-09 00:42:26 +0000

    Added metric_id field.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@603 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/app/models/task.php
+++ trunk/PHP/app/models/task.php
@@ -5,13 +5,23 @@ class Task extends AppModel {
 		'name' => array(
 			'notempty' => array(
 				'rule' => array('notempty'),
-	//'message' => 'Your custom message here',
-	//'allowEmpty' => false,
-	//'required' => false,
-	//'last' => false, // Stop validation after this rule
-	//'on' => 'create', // Limit validation to 'create' or 'update' operations
-	),
-	),
+			//'message' => 'Your custom message here',
+			//'allowEmpty' => false,
+			//'required' => false,
+			//'last' => false, // Stop validation after this rule
+			//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			),
+		'metric_id' => array(
+			'notempty' => array(
+				'rule' => array('numeric'),
+			//'message' => 'Your custom message here',
+			//'allowEmpty' => false,
+			//'required' => false,
+			//'last' => false, // Stop validation after this rule
+			//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+			)
 	);
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
 
