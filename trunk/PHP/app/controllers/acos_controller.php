ed04bbff3cfc2211678d9813be22045531299585
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-05-22 19:50:41 +0000

    Initial Load.
    Added this controller in case the acos table needs to rebuilt.
    
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@684 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

---
+++ trunk/PHP/app/controllers/acos_controller.php
@@ -0,0 +1,18 @@
+<?php
+class AcosController extends AppController {
+
+	var $name = 'Acos';
+	
+	function beforeFilter() {
+		parent::beforeFilter();
+		
+		//$this->Auth->allow('*');
+	}
+	
+	function recoverTree(){
+		$this->Aco->recover('parent');
+	}
+
+	
+}
+?>
