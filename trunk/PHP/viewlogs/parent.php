9d5c8aee473e09e760db4c00aa428f26da0a3840
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2011-09-27 02:22:30 +0000

    added order url parameter
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@764 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/viewlogs/parent.php
+++ trunk/PHP/viewlogs/parent.php
@@ -1,12 +1,40 @@
 <html>
 <body>
 
-<a href="./displaylog.php?log=log4jError.log">Error</a><BR>
-<a href="./displaylog.php?log=log4jFull.log">Full</a><BR>
-<a href="./displaylog.php?log=log4jSQL.log">SQL</a><BR>
-<a href="./displaylog.php?log=log4jConsole.log">Console</a><BR>
-<a href="./displaylog.php?log=log4jThread.log">Thread</a><BR>
-<a href="./displaylog.php?log=log4jApache.log">Apache</a>
+
+<table>
+<tr><td>
+<a href="./displaylog.php?log=log4jError.log">Error Logs (backward)</a>
+</td><td>
+<a href="./displaylog.php?log=log4jError.log&order=forward">Error Logs (forward)</a>
+</td></tr>
+<tr><td>
+<a href="./displaylog.php?log=log4jFull.log">Full Logs (backward)</a>
+</td><td>
+<a href="./displaylog.php?log=log4jFull.log&order=forward">Full Logs (forward)</a>
+</td></tr>
+<tr>
+<td>
+<a href="./displaylog.php?log=log4jSQL.log">SQL Logs (backward)</a>
+</td><td>
+<a href="./displaylog.php?log=log4jSQL.log&order=forward">SQL Logs (forward)</a>
+</td></tr>
+<tr><td>
+<a href="./displaylog.php?log=log4jConsole.log">Console Logs (backward)</a>
+</td><td>
+<a href="./displaylog.php?log=log4jConsole.log&order=forward">Console Logs (forward)</a>
+</td></tr>
+<tr><td>
+<a href="./displaylog.php?log=log4jThread.log">Thread Logs (backward)</a>
+</td><td>
+<a href="./displaylog.php?log=log4jThread.log&order=forward">Thread Logs (forward)</a>
+</td></tr>
+<tr><td>
+<a href="./displaylog.php?log=log4jApache.log">Apache Logs (backward)</a>
+</td><td>
+<a href="./displaylog.php?log=log4jApache.log&order=forward">Apache Logs (forward)</a>
+</td></tr>
+</table>
 </body>
 
 
