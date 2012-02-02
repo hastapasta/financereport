a6db9956dc6abaf27127ccca7823bd0deeb8d5f7
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2012-02-02 20:11:50 +0000

    Initial import.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@878 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

---
+++ trunk/PHP/translate/translate/translate.php
@@ -0,0 +1,72 @@
+
+<html>
+<head>
+<!-- 
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-26582307-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+-->
+<script>
+function googleSectionalElementInit() {
+  new google.translate.SectionalElement({
+    sectionalNodeClassName: 'goog-trans-section',
+    controlNodeClassName: 'goog-trans-control',
+    background: '#f4fa58'
+  }, 'google_sectional_element');
+}
+</script>
+<script src="//translate.google.com/translate_a/element.js?cb=googleSectionalElementInit&ug=section&hl=en"></script>
+</head>
+<body>
+
+
+
+This is some text.
+
+<div class="goog-trans-section">
+The quick brown fox jumped over the lazy dog.
+</div>
+
+<p><div class="goog-trans-section" lang="en">
+<div class="goog-trans-control"></div>
+This is an example web page. You can preview and test the website translator here. Try changing the above settings and click the "Preview and Try!" button to see the effect. For example, if you select specific translation languages, the languages in the dropdown menu will change. Enjoy!
+</div>
+<p>
+
+This is some text.
+
+Esta es una página web de ejemplo. Puedes obtener una vista previa de la página y probar el traductor de sitios web desde aquí. Prueba a cambiar las opciones anteriores y haz clic en el botón "Obtener vista previa y probar" para ver cómo queda la página. Por ejemplo, si seleccionas idiomas de traducción específicos, los idiomas del menú desplegable cambiarán. ¡No esperes para probar!
+
+
+<div class="goog-trans-control"></div>
+<div class="goog-trans-section" lang="es">
+<textarea rows="5" cols="20">
+I was watching my TV when a man came on the television.
+Telling me how white my shirts could be.
+He can't be a man because he doesn't smoke the same cigarettes as me.
+
+</textarea>
+</div>
+<script>
+function googleSectionalElementInit() {
+  new google.translate.SectionalElement({
+    sectionalNodeClassName: 'goog-trans-section',
+    controlNodeClassName: 'goog-trans-control',
+    background: '#f4fa58'
+  }, 'google_sectional_element');
+}
+</script>
+<script src="//translate.google.com/translate_a/element.js?cb=googleSectionalElementInit&ug=section&hl=en"></script>
+
+</body>
+</html>
---
+++ trunk/PHP/translate/translate/translate3.php
@@ -0,0 +1,160 @@
+<?php
+
+include ("../../site/includes/sitecommon.php");
+
+
+
+?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en"  dir=ltr>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=utf-8">
+<?php IncFunc::jQuery();?>
+
+
+<script>
+$(document).ready(function() {
+<?php 
+$text = "";
+if(isset($_POST['submit_msg'])) {
+	$text = $_POST['textarea1'];
+	echo " $(\"#textarea1\").val(\"".$text."\");";
+}
+
+
+
+
+?>
+
+});
+</script>
+</head>
+
+<body style="font-family:arial;font-size:10pt">
+
+<table width="100%"><tr><td style="padding-left:10px;padding-right:10px;">
+
+<p>
+<?php 
+include ("../../common/functions.php");
+
+db_utility::setDatabase("translate");
+db_utility::db_connect();
+
+
+if (isset($_POST['val1'])) {
+	
+	$query1 = "insert into fact_data (text_original,text_translate,date_collected,source_id) values ";
+	$query1.= "('".str_replace('\'','\\\'',$_POST['val1'])."','".str_replace('\'','\\\'',$_POST['val2'])."',NOW(),".$_POST['sourceval'].")";
+	$result1 = mysql_query($query1) or die("Failed Query of " . $query1);
+	
+	echo "New translation was published.<BR>";
+	echo "<table>";
+	echo "<tr><td>original: </td><td>".$_POST['val1']."</td></tr>";
+	echo "<tr><td>translation: </td><td>".$_POST['val2']."</td></tr>";
+	
+	$query2 = "select name from sources where id=".$_POST['sourceval'];
+	$result2 = mysql_query($query2) or die("Failed Query of " . $query2);
+	$row2 = mysql_fetch_array($result2);	
+	echo "<tr><td>source: </td><td>".$row2['name']."</td></tr>";
+	echo "</table>";
+}
+
+
+
+?>
+
+<p>
+<p>
+<div class="goog-trans-section" lang="es">
+<div  class="goog-trans-control"></div>
+<div id="translated">
+<?php echo $text; ?>
+</div>
+</div>
+</td></tr></table>
+
+<script>
+<?php echo "var originalText='".$text."';"; ?>
+function googleSectionalElementInit() {
+  new google.translate.SectionalElement({
+    sectionalNodeClassName: 'goog-trans-section',
+    controlNodeClassName: 'goog-trans-control',
+    background: '#f4fa58'
+  }, 'google_sectional_element');
+}
+
+function publish() {
+	val1 = document.getElementById("val1");
+
+	originalText = originalText.replace(/(\r\n|\n|\r)/gm,"");
+	val1.value = originalText;
+	//alert(val1.value);
+	
+
+	val2 = document.getElementById("val2");
+	transval = document.getElementById("translated");
+
+	
+	
+	
+	tmptext = strip(transval.innerHTML);
+	tmptext = tmptext.replace(/(\r\n|\n|\r)/gm,"");
+	//alert(tmptext);
+	val2.value = tmptext;
+
+	sourceval = document.getElementById("sourceval");
+	sourceval.value = $("#sources").val();
+
+	//alert("submitting");
+	document.forms["publishform"].submit();
+	
+	
+}
+
+function strip(html) {
+   var tmp = document.createElement("DIV");
+   tmp.innerHTML = html;
+   return tmp.textContent||tmp.innerText;
+}
+
+
+</script>
+<script src="//translate.google.com/translate_a/element.js?cb=googleSectionalElementInit&ug=section&hl=en"></script>
+
+
+	
+<form action="translate3.php" method=POST > 
+<textarea name="textarea1" id="textarea1" rows="5" cols="100">
+</textarea>
+<BR><BR>
+<input type="submit" value="   Submit   " name="submit_msg" >
+</form>
+
+<BR><BR>
+<input type="button" style="float: left;clear: both;" value="  Publish  "
+	onclick="publish();return false;">
+	
+<form name="publishform" action="translate3.php" method=POST>
+<input type="hidden" name="val1" id="val1" value="">
+<input type="hidden" name="val2" id="val2" value="">
+<input type="hidden" name="sourceval" id="sourceval" value="">
+<BR><BR>
+Source: 
+<select id="sources" style="background-color: #FFFFFF">
+
+<?php 
+	$sql = "select name,id from sources ";
+	$result = mysql_query($sql);
+	for ($j=0;$j<mysql_num_rows($result);$j++)	{
+		$row1 = mysql_fetch_array($result);		
+		echo "<option value=".$row1['id'].">".$row1['name']."</option>";
+	}	
+?>
+</select>
+</form>
+
+
+
+</body></html>
