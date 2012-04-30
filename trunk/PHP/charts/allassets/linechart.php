4deade7b955add9e344f404f0d7e0764e8cf1900
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2012-04-30 04:49:17 +0000

    -made changes for new datasource structure (mysqldatasource15single and mysqldatasource15multiple) -added checkbox for percent change -merged linechart.php and linechart15min.php
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@935 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/charts/allassets/linechart.php
+++ trunk/PHP/charts/allassets/linechart.php
@@ -2,7 +2,7 @@
 //require_once 'init2.php';
 
 require_once '../../common/functions.php';
-include ("../../site/includes/sitecommon.php");
+require_once ("../../site/includes/sitecommon.php");
 
 db_utility::db_connect();
 
@@ -18,12 +18,20 @@ else
 	$error =  "No alert id,entity id  or ticker parameter in url. Unable to render chart.";
 	//die("No alert id,entity id  or ticker parameter in url. Unable to render chart.");
 	
+$gran="day";
+if (isset($_GET['gran']))
+	$gran= $_GET['gran'];
+	
 $metricid="0";
 if (isset($_GET['m']))
 	$metricid = $_GET['m'];
 	
-if (!empty($alertid))
-{
+$percent=false;
+if (isset($_GET['percent']))
+	if (strcasecmp('true', $_GET['percent'])==0)
+		$percent=true;
+	
+if (!empty($alertid)) {
 	$sql1 = "select entity_id from alerts where id=".$alertid;
 	$result1 = mysql_query($sql1);
 	$row1 = mysql_fetch_array($result1);
@@ -52,7 +60,21 @@ if (!empty($ticker)) {
 	$entityid=substr($entityid,0,strlen($entityid)-1);
 }
 
-$begindate="2011-01-01";
+if (empty($begindate)) {
+	$begindate="2011-01-01";
+	if ($gran=='minute') {		
+		//$date = date('Y-m-d');
+		$date = "2012-03-01";
+		$newdate = strtotime ( '-1 month' , strtotime ( $date ) ) ;
+		$begindate = date ( 'Y-m-d' , $newdate );
+		
+	}
+	
+	
+	
+	
+}
+
 $enddate="";
 
 if (isset($_GET['begindate']) && !empty($_GET['begindate']))
@@ -136,7 +158,9 @@ if (isset($_GET['group']))
 </style>
    
     <script type="text/javascript">
-
+    	$(document).ready(function() {
+        	$("#checkbox1").attr("checked", <?php echo ($percent == true ? 'true' : 'false');?>); 
+    	});
     	$(function(){
     		$( "#a_c" ).autocomplete({
     			source: function( request, response ) {
@@ -170,17 +194,16 @@ if (isset($_GET['group']))
     			select: function( event, ui ) {
     				if(ui.item)	{    
         				
-        				//alert(ui.item.id);
-        				//sendAndDraw(ui.item.id,ui.item.label,ui.item.full_name);
+        				redraw(ui.item.id);
         				<?php
-        						echo "window.location.replace(\"";
-        						echo IncFunc::$PHP_ROOT_PATH;
-        						echo "/charts/allassets/linechart.php?type=";
-        						echo $type;
+        						//echo "window.location.replace(\"";
+        						//echo IncFunc::$PHP_ROOT_PATH;
+        						//echo "/charts/allassets/linechart.php?type=";
+        						//echo $type;
         						/*
         						 * There are other javascript enocde functions: escape() and encodeURIComponent()
         						 */
-        						echo "&e=\"+encodeURI(ui.item.id));";
+        						//echo "&e=\"+encodeURI(ui.item.id)+\"&percent=\"+encodeURI($(\"#checkbox1\").is(':checked')));";
         						
         				?>
     				}
@@ -190,26 +213,47 @@ if (isset($_GET['group']))
         		if (e.which == 13) { 
             		//yourFunction($('#yourAutoComplete').val()); return false;
             		value = $("#a_c").val();
+            		redraw(value);
       
 					<?php
-						echo "window.location.replace(\"";
-						echo IncFunc::$PHP_ROOT_PATH;
-						echo "/charts/allassets/linechart.php?title=";
-						echo urlencode($title);
+						//echo "window.location.replace(\"";
+						//echo IncFunc::$PHP_ROOT_PATH;
+						//echo "/charts/allassets/linechart.php?title=";
+						//echo urlencode($title);
 						/*
 						 * There are other javascript enocde functions: escape() and encodeURIComponent()
 						 */
-						echo "&t=\"+encodeURI(value));";
+						//echo "&t=\"+encodeURI(value));";
 						
 					?>
-					
-					
-					
-					
+		
         			//sendAndDraw(ui.item.id,ui.item.label,ui.item.full_name); 
             	} 
         	}); 
+        	$('#checkbox1').click(function () {
+				//alert('clicked');
+				//alert($("#checkbox1").is(':checked'));
+				<?php echo "redraw('".$entityid."');\n"; ?>
+        	});
         });
+
+        function redraw(value) {
+            //alert('in redraw');
+        	<?php
+				echo "window.location.replace(\"";
+				echo IncFunc::$PHP_ROOT_PATH;
+				echo "/charts/allassets/linechart.php?&gran=".$gran;
+				//echo urlencode($title);
+				/*
+				 * There are other javascript enocde functions: escape() and encodeURIComponent()
+				 */
+				//echo "&e=\"+encodeURI(value));";
+				echo "&e=\"+encodeURI(value)+\"&percent=\"+encodeURI($(\"#checkbox1\").is(':checked')));";
+					
+			?>
+
+
+        }
 
         
     
@@ -224,7 +268,6 @@ if (isset($_GET['group']))
 
         
         function sendAndDraw(id,ticker,fullname) {
-
             
 
             /*var chart2 = document.getElementById('orgchart2');
@@ -236,49 +279,49 @@ if (isset($_GET['group']))
 
             
             var options = {};
-         	//options['title'] = 'Cotton Futures';
-         	/*options['height'] = 600;
-         	options['width'] = 900;
-         	options['fontSize'] = 15;
-         	//options['curveType'] = 'function';
-         	options['pointSize'] = 0;
-			options.interpolateNulls = true;
-			options.backgroundColor = {};
-			options.backgroundColor.fill = 'white';
-			options.backgroundColor.stroke = '#000';
-         	options.backgroundColor.strokeWidth = 8;*/
 
          	options.scaleType='allmaximixed';
          	options.scaleType='maximized';
          	options.wmode='opaque';
          	options.thickness=2;
          	options.legendPosition='newRow';
-         	
-            
 
-
-
-
-        	
-        	
         	/*
         	* metric id of zero means use the default.
         	*/
 			<?php 
 			if (sizeof($entities) > 1) {
-				echo "options.allValuesSuffix='%';\n";
+			//if ($percent == true) {
 				echo "var queryPath = '".IncFunc::$JSP_ROOT_PATH."mysqldatasource15multiple.jsp?begindate=".$begindate;
 				if (!empty($enddate))
 					echo "&enddate=".$enddate;
-				echo "&alertid=0&metricid=0';\n";
+				/* OFP 4/29/2012 - Right now multiple entities doesn't support anything other
+				 * than the default metric id.
+				 */
+				echo "&alertid=0&metricid=0";
 				
 			}
 			else {
-				echo "var queryPath = '".IncFunc::$JSP_ROOT_PATH."mysqldatasource15.jsp?begindate=".$begindate;
+				echo "var queryPath = '".IncFunc::$JSP_ROOT_PATH."mysqldatasource15single.jsp?begindate=".$begindate;
 				if (!empty($enddate))
 					echo "&enddate=".$enddate;
-					echo "&alertid=0&metricid=".$metricid."';\n";
+				echo "&alertid=0&metricid=".$metricid;
 			}
+			
+			if ($percent==true)
+				echo "&percent=true";
+			else
+				echo "&percent=false";
+				
+			if ($gran=="minute")
+				echo "&gran=minute";
+			else 
+				echo "&gran=day";
+				
+			echo "';\n";
+			
+			if ($percent==true)
+				echo "options.allValuesSuffix='%';\n";
 			
 			?>
         	title = document.getElementById('chart-title');
@@ -319,7 +362,7 @@ if (isset($_GET['group']))
             var lineChart1 = new google.visualization.AnnotatedTimeLine(container1);
             //var tableChart2 = new google.visualization.Table(container2);
             
-         
+         	//alert(queryPath);
             if (window.console && window.console.firebug) {console.log(queryPath)}
 
             
@@ -340,7 +383,22 @@ if (isset($_GET['group']))
     </script>
 <?php IncFunc::googleAnalytics();?>
 </head>
-<body id="page1">
+
+<?php 
+
+/*
+ * I don't like controlling the tabs using the body tag. Should control it near the tabs
+ * themselves.
+ */
+
+if ($gran=='minute')
+	echo "<body id=\"page2\">";
+else
+	echo "<body id=\"page1\">";
+
+
+?>
+
 <div id="jq-siteContain">
 
 <?php 
@@ -368,19 +426,37 @@ if (isset($_GET['group']))
     <BR><BR>
     Enter entity name (stock ticker, equity index, currency cross, etc):
     <BR>
-  	<input type='text' id='a_c' style='z-index:3' /><br/>
-  	<BR>	
-  	<div style="font-size:30;margin: 10px 0 0 0;" id="chart-title"></div>
-  	<BR>
+  	<input type='text' id='a_c' style='z-index:3;clear:both;float:left;' />
+  	<div style='z-index:3;margin-bottom: 10px;float:left;clear:both;margin-top: 10px;' >
+  	Y axis values in terms of % change: 
+  	<input type='checkbox' id='checkbox1' />
+  	</div>
+  	<div style="font-size:30;margin: 10px 0 20px 0;float:left;clear:both;" id="chart-title"></div>
 	</div>
 	
-	<div style="font-size:20" id="charttabs">
+	<div style="font-size:20;clear:both;" id="charttabs">
+	
+	
+	<?php 
+		$serverquery = $_SERVER['QUERY_STRING'];
+		
+		parse_str($serverquery, $params);
+		/*
+		 * $params = array(
+		 *     'mode'    => '1'
+		 *     'newUser' => '1'
+		 * )
+		 */
+		unset($params['gran']);
+		$serverquery = http_build_query($params);
+		
+	?>
 	<ul id="tabs">
-	<li id="tab1"><a href=<?php echo "\"".IncFunc::$PHP_ROOT_PATH."/charts/allassets/linechart.php?".$_SERVER['QUERY_STRING']."\""?>>Day Granularity</a></li>
-	<li id="tab2"><a href=<?php echo "\"".IncFunc::$PHP_ROOT_PATH."/charts/allassets/linechart15min.php?".$_SERVER['QUERY_STRING']."\""?>>Quarter Hour Granularity</a></li>
+	<li id="tab1"><a href=<?php echo "\"".IncFunc::$PHP_ROOT_PATH."/charts/allassets/linechart.php?".$serverquery."\""?>>Day Granularity</a></li>
+	<li id="tab2"><a href=<?php echo "\"".IncFunc::$PHP_ROOT_PATH."/charts/allassets/linechart.php?".$serverquery."&gran=minute\""?>>Quarter Hour Granularity</a></li>
 	</ul>
 	</div>
-    <div id="chart-div" style="margin: 30px 0 20px 0;width:800px;height:600px"></div>
+    <div id="chart-div" style="margin: 30px 0 20px 0;width:800px;height:600px;float:left;clear: both;"></div>
 
  
 </div> <!--  pf-body -->
