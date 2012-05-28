f16994bd7fc23a795794fe14485f4b8705f1261b
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2012-05-28 20:16:49 +0000

    Initial load.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@963 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

---
+++ trunk/PHP/site/includes/pftable.js
@@ -0,0 +1,14 @@
+var TableObject = function () {
+	this.container = "";
+	this.dataSourceUrl = "";
+	this.query = "";
+	this.tableChart = "";
+	this.queryWrapper = "";
+	this.handler = "";
+		
+
+};
+
+/*TableObject.prototype.setTableChart = function(tableChart) {
+	this.tableChart = tableChart;
+}*/
