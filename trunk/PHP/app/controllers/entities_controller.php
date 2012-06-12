539c73573d78a7b6bb30a82acd0233d96fc762ba
hastapasta99 <hastapasta99@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2012-06-12 05:29:05 +0000

    -many changes, modified filters and added split dialog
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@987 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/PHP/app/controllers/entities_controller.php
+++ trunk/PHP/app/controllers/entities_controller.php
@@ -2,7 +2,7 @@
 class EntitiesController extends AppController {
 
 	var $name = 'Entities';
-	var $uses = array('Entity','Alert','User','EntityGroup');
+	var $uses = array('Entity','Alert','User','EntityGroup','Metric','Country','CountriesEntity');
 	
 	function beforeFilter() {
 		parent::beforeFilter();
@@ -10,22 +10,86 @@ class EntitiesController extends AppController {
 	}
 
 	function index() {
-		$this->urlToNamed();
-		$this->Entity->recursive = 0;
-		$conditions = array();
+		//$this->urlToNamed();
+		$this->Entity->recursive = 1;
+		//$conditions = array();
+		//debug($this->passedArgs);
 		
+		$filtervalues = $this->data;
+		$conditions = $this->Session->read('entityfilterconditions');
 		
-		if(isset($this->passedArgs['enable']) && $this->passedArgs['enable'] == 1){
-			if( isset($this->passedArgs['ticker']) && !empty($this->passedArgs['ticker']))
-				$conditions['Entity.ticker LIKE'] = "%".$this->passedArgs['ticker']."%";
-			if(isset($this->passedArgs['name']) && !empty($this->passedArgs['name']))
-				$conditions['Entity.full_name LIKE'] = "%".$this->passedArgs['name']."%";
-			if(isset($this->passedArgs['country_id']) && !empty($this->passedArgs['country_id']))
-				$conditions['Entity.country_id'] = $this->passedArgs['country_id']; 
-		}
+		/*
+		 * $this->Recipe->bindModel(array('hasOne' => array('RecipesTag',
+    'FilterTag' => array(
+        'className' => 'Tag',
+        'foreignKey' => false,
+        'conditions' => array('FilterTag.id = RecipesTag.tag_id')
+))));
+$this->Recipe->find('all', array(
+    'fields' => array('Recipe.*'),
+    'conditions' => array('FilterTag.name' => 'Dessert')
+));
+
+		 * 
+		 * 
+		 * 
+		 */
+		
+		/*
+		 * This returns the data, now need to get the contain working.
+		 */
+		//$blap = $this->Entity->Country->find('first', array('conditions' => array('Country.id' => 78)));
+		//var_dump($blap);
+		/*
+		 * 
+		 */
+		
+		/*'contain' => array('Amenity'
+=> array('UsersAmenities')));*/
+		
+		//$this->Entity->Country->contain(array('fields'=>'Entity.ticker'));
+		//$blap = $this->Entity->Country->find('first', array('conditions' => array('Country.id' => 78),'contain'=> array('Entity'=>array('ticker'))));
+		//var_dump($blap);
+		
+		//$this->Entity->Country->contain(array('fields' => 'ticker'));
+		//$this->Entity->contain('ticker');
+		//$blap = $this->Entity->find('all', array('fields'=> 'Entity.ticker','conditions'=>array('Entity.id'=>78)));
+		
+		//$blap = $this->Entity->Country->find('first', array('conditions' => array('Country.id' => 78),'contain'=>array('fields'=>'Entity.ticker')));
+		//var_dump($blap);
+		//$countyent=$this->
+		//debug($filtervalues);exit;
+		if(!empty($filtervalues)){
+			$conditions="";
+			if(isset($filtervalues['Entity']['enable']) && $filtervalues['Entity']['enable'] == 1){
+				if( isset($filtervalues['Entity']['ticker']) && !empty($filtervalues['Entity']['ticker']))
+					$conditions['Entity.ticker LIKE'] = $filtervalues['Entity']['ticker'];
+				if(isset($filtervalues['Entity']['name']) && !empty($filtervalues['Entity']['name']))
+					$conditions['Entity.full_name LIKE'] = $filtervalues['Entity']['name'];
+				if(isset($filtervalues['Country']['name']) && !empty($filtervalues['Country']['name'])) {
+					$conditions1['CountriesEntity.CountryId'] = $filtervalues['Country']['name'];
+					
+					$entityId=$this->CountriesEntity->findAllByCountryId($conditions1,array('CountriesEntity.entity_id'));
+					var_dump($entityId);
+					$conditions['Entity.id']=$entityId;
+					//var_dump('here');G
+				}
+				//var_dump($conditions);
+				//var_dump($this->data);
+					
+				
+			}	
 			
-		//debug($conditions);
-		$this->set('entities', $this->paginate($conditions));
+			$this->Session->write('entityfilterconditions',$conditions);
+			$this->Session->write('entityfiltervalues',$filtervalues);
+		}
+		
+
+		$this->paginate = array_merge( $this->paginate,  array('conditions' => $conditions));
+
+		$countries=$this->Country->find('list',array('fields'=>array('id','name'),'order'=>'Country.name asc'));
+		$this->set(compact('countries'));
+		$this->set('entities', $this->paginate(null,$conditions));
 	}
 
 	function view($id = null) {
@@ -57,7 +121,7 @@ class EntitiesController extends AppController {
 				$this->Session->setFlash(__('The entity has been saved', true),'default',array('class'=>'green_message'));
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The entity could not be saved. Please, try again.', true));
+				$this->Session->setFlash(__('The entity cound not be saved. Please see below for additional information.', true));
 			}
 		}
 	}
@@ -71,7 +135,7 @@ class EntitiesController extends AppController {
 				$this->Session->delete('Record');
 				$this->redirect(array('action' => 'index'));
 			} else {
-				$this->Session->setFlash(__('The Entities could not be saved. Please, try again.', true));
+				$this->Session->setFlash(__('The Entities cound not be saved. Please see below for additional information.', true));
 			}
 		}
 		if (!empty($record)) {
@@ -81,6 +145,9 @@ class EntitiesController extends AppController {
 			$this->Session->setFlash(__('Invalid Entities', true));
 			$this->redirect(array('action' => 'index'));
 		}	
+		
+		$metric_names = $this->Metric->find('list', array('fields'=> 'name'));
+		$this->set('metric_names', $metric_names);
 	}
 
 	function delete($id = null) {
@@ -100,12 +167,69 @@ class EntitiesController extends AppController {
 		$this->autoRender = false;
 		$this->layout = null;
 		$term  =  $_REQUEST['term'];
-		$this->loadModel("Country");
+		//$this->loadModel("Country");
 		$conditions = array();
 		$conditions['Country.name LIKE'] = $term."%";
-		$countries = $this->Country->find('all', array('conditions'=>$conditions, 'order'=>'name ASC', 'fields'=>array('id','name')));
+		$countries = $this->Country->find('all', array('conditions'=>$conditions, 'recursive'=>0,'order'=>'name ASC', 'fields'=>array('Country.id','Country.name')));
 		$this->set("data", $countries);
+		//var_dump($countries);
+		//var_dump($countries);
 		$this->render('json');	
+	}
+	
+	function split(){
+		
+		
+		$ratio = $this->params['url']['ratio'];
+		$entity_id = $this->params['url']['entity_id'];
+		$datecutoff = $this->params['url']['datecutoff'];
+		
+		//$sql1 -> insert split operation into splits table for tracking purposes
+		//$sql2 -> update metric_id=1 (price) values in fact_data table
+		//$sql3 -> update metric_id=4,5,7 & 8 (eps actual & estimate, annual and quarterly) in fact_data
+		//$sql4 -> update shares outstanding in entities table
+		
+		$array = split(":",$ratio);
+		
+		$sql1 = "insert into splits (date_issued,entity_id,ratio,cutoff_date) ";
+		$sql1.= " values (NOW(),".$entity_id.",'".$ratio."','".$datecutoff."')";
+		
+		$sql2 = "update fact_data set value = round(value * ".$array[1]." / ".$array[0].",3) ";
+		$sql2.= " where entity_id=".$entity_id." and metric_id=1 ";
+		$sql2.= " AND date_collected < '".$datecutoff."' ";
+		
+		$sql3 = "update fact_data set value = round(value * ".$array[1]." / ".$array[0].",3) ";
+		$sql3.= " where entity_id=".$entity_id." and metric_id in (4,5,7,8) ";
+		$sql3.= " AND date_collected < '".$datecutoff."' ";
+		
+		$sql4 = "update entities set shares_outstanding = round(shares_outstanding * ".$array[0]." / ".$array[1].",0) ";
+		$sql4.= " where id=".$entity_id;
+		
+		
+		//debug($sql1,true);
+		//debug($sql2,true);
+		//debug($sql3,true);
+		//debug($sql4,true);	
+	
+		$data1 = $this->Entity->query($sql1);
+		$this->set('sql1', $data1);
+		//debug($data1);
+		
+		$data2 = $this->Alert->query($sql2);
+		$this->set('sql2', $data2);
+		//debug($data2);
+		
+		
+		$data3 = $this->Alert->query($sql3);
+		$this->set('sql3', $data3);
+		//debug($data3);
+		
+		
+		
+		$data4 = $this->Alert->query($sql4);
+		$this->set('sql4', $data4);
+		//debug($data4);
+
 	}
 }
 ?>
