c9e1a0866e629d72750ecb520883c82cc7d00eed
hastapasta99@gmail.com <hastapasta99@gmail.com@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2012-11-16 19:14:42 +0000

    In the process of reorganizing branches.
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@1179 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/Java/DataLoadSpringOllieLocal/src/com/pikefin/exceptions/CustomRegexException.java
+++
@@ -1,6 +0,0 @@
-package com.pikefin.exceptions;
-
-public class CustomRegexException extends Exception {
-
-	private static final long serialVersionUID = 3833467121806555695L;
-}
--- trunk/Java/DataLoadSpringOllieLocal/src/com/pikefin/exceptions/TagNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.pikefin.exceptions;
-
-public class TagNotFoundException extends Exception {
-	private static final long serialVersionUID = 3350211930488258897L;
-
-	void TagNoutFoundException() {
-
-	}
-}
