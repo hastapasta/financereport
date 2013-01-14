8061cb22981ecbfe88eea1e774140d01bdcb022c
hastapasta99@gmail.com <hastapasta99@gmail.com@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2013-01-14 00:14:26 +0000

    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@1380 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- branches/MergeTest/src/com/pikefin/services/inter/LogTaskService.java
+++
@@ -1,9 +0,0 @@
-package com.pikefin.services.inter;
-
-import com.pikefin.businessobjects.LogTask;
-import com.pikefin.exceptions.GenericException;
-
-public interface LogTaskService {
-	public LogTask saveLogTaskInfo(LogTask logTaskEntity) throws GenericException;
-
-}
