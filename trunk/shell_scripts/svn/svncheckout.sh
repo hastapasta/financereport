36d26f6a03a5bc61f834e5d39db0a13fec7590a1
hastapasta99@gmail.com <hastapasta99@gmail.com@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2013-02-11 02:17:32 +0000

    add to subversion
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@1411 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

---
+++ trunk/shell_scripts/svn/svncheckout.sh
@@ -0,0 +1,8 @@
+#svn checkout https://financereport.googlecode.com/svn/FinancialReport/trunk/PHP/cake/ --username hastapasta99@gmail.com --password ns6Ms5ar8hc9 --non-recursive
+#svn checkout https://www.testpikefin.com/svn/financereport/FinancialReport/trunk/Java/PDI/ --username tmpusr --password Test123 
+#svn copy https://www.pikefin.com/svn/financereport/FinancialReport/trunk/Java/BuildViews/ https://www.pikefin.com/svn/financereport/FinancialReport/trunk/Java/BuildViewsCopy/ --username vishal --password vishal135 
+#svn cp https://www.testpikefin.com/svn/financereport/FinancialReport/trunk \https://www.pikefin.com/svn/financereport/FinancialReport/branches/test1
+#svn checkout https://financereport.googlecode.com/svn/FinancialReport/trunk/libraries/java/aopalliance /home/ollie/workspace/libraries/java/aopalliance
+svn checkout https://financereport.googlecode.com/svn/FinancialReport/trunk/libraries $1 
+#svn checkout https://financereport.googlecode.com/svn/FinancialReport/trunk/Java/DataLoadEagle /tmp/DataLoadEagle 
+
