136405747faed502693a59c380b1bd409f58a5c4
hastapasta99@gmail.com <hastapasta99@gmail.com@d2ea8a1b-09b1-a710-b10d-4e27fa839f9d>
2013-02-11 01:47:43 +0000

    -remove passwords and outdated connect commands
    git-svn-id: http://financereport.googlecode.com/svn/FinancialReport@1410 d2ea8a1b-09b1-a710-b10d-4e27fa839f9d

--- trunk/shell_scripts/connectmysql.sh
+++ trunk/shell_scripts/connectmysql.sh
@@ -1,5 +1,3 @@
-#mysql -h 5.213.143.108 -u root -pmadmax1. -e "show variables;" > /tmp/mysqlgodadvar
-#mysql -hfindata.clpkcufglfdn.us-east-1.rds.amazonaws.com -ufinuser -pq1w2e3r4 -e "show variables;" > /tmp/mysqlawsvar
-#mysql -h 5.213.143.108 -u root -pmadmax1.
-#mysql -hfindata.clpkcufglfdn.us-east-1.rds.amazonaws.com -ufinuser -pq1w2e3r4
-mysql -h testdataload -u root -pmadmax1.
+#mysql -h testdataload -u root -password
+#mysql -h pikefin3ham -u root -password
+mysql -h localhost -u root -password
